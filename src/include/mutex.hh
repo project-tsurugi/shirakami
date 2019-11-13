179742ee9f2b95ca70735e7d699114751f1f7347
thawk105 <thawk105@gmail.com>
2019-11-13 14:45:47 +0900

    new file creation
    src/include/clock.hh, src/include/mutex.hh

---
+++ src/include/mutex.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "include/header.h"
+#include "kvs/debug.h"
+
+namespace kvs {
+
+void
+lock_mutex(pthread_mutex_t *mutex)
+{
+  int ret;
+  ret = pthread_mutex_lock(mutex);
+  if (ret != 0) ERR;
+}
+
+void
+unlock_mutex(pthread_mutex_t *mutex)
+{
+  int ret;
+  ret = pthread_mutex_unlock(mutex);
+  if (ret != 0) ERR;
+}
+
+}  // namespace kvs
