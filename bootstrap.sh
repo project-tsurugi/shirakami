399a8ef35b81e6dccefa91e8552ed7c47843c6f4
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2020-04-14 18:26:20 +0900

    bootstrap.sh make masstree library and delete codes corresponding to CMakeLists.txt.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ cd third_party/masstree-beta
 ./configure
 make clean
 make -j CXXFLAGS='-g -W -Wall -O3 -fPIC'
+ar cr libjson.a json.o string.o straccum.o str.o msgpack.o clp.o kvrandom.o compiler.o memdebug.o kvthread.o misc.o
+ranlib libjson.a
 cd ../../
 
 # This directory will be used for logging.
