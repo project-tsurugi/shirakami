ee2df3d4007abcfe0475fcc9bde90c6a56fcaec6
Takashi Horikawa <horikawa@nautilus-technologies.com>
2020-04-14 10:34:54 +0900

    use common routine and implement a loop

--- test/scanPerfTest.cpp
+++ test/scanPerfTest.cpp
@@ -15,259 +15,148 @@ using std::endl;
 
 namespace kvs_charkey::testing {
 
+const bool exc_kernel = false;
+
 class ScanPerfTest : public ::testing::Test {
- protected:
+protected:
   ScanPerfTest() { kvs::init(); }
   ~ScanPerfTest() { 
     kvs::fin(); 
     kvs::delete_all_records();
     kvs::delete_all_garbage_records();
   }
+  void DoScan(int);
+
+  const size_t MAX_TUPLES = 1000000;
+  const size_t READ_TUPLES = 100;
+  uint64_t start, end, scan_size;
+  Token s{};
+  Storage st{};
 };
 
-TEST_F(ScanPerfTest, read_from_scan) {
-  uint64_t key[1000000];
-  for (int i = 0; i < 1000000; ++i) {
-    key[i] = i;
-  }
-  std::string v1("a");
-  Token s{};
-  ASSERT_EQ(Status::OK, enter(s));
-  Storage st{};
-  uint64_t start, end, scan_size;
-
-  for (int i = 0; i < 100; ++i) {
-    ASSERT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
-  }
-  ASSERT_EQ(Status::OK, commit(s));
-
-  cout << "Perform 100 records read_from_scan on a table with 100 records." << endl;
+void
+ScanPerfTest::DoScan(int loop)
+{
   ScanHandle handle{};
   Tuple* tuple{};
   {
-    PerfCounter perf_counter(true);
+    PerfCounter perf_counter(exc_kernel);
     perf_counter.start();
     start = rdtscp();
-    ASSERT_EQ(Status::OK, open_scan(s, st, nullptr, 0, false, nullptr, 0, false, handle));
-    for (int i = 0; i < 100; ++i) {
-      ASSERT_EQ(Status::OK, read_from_scan(s, st, handle, &tuple));
+    for (int j = 0; j < loop; ++j) {
+      EXPECT_EQ(Status::OK, open_scan(s, st, nullptr, 0, false, nullptr, 0, false, handle));
+      for (int i = 0; i < READ_TUPLES; ++i) {
+        EXPECT_EQ(Status::OK, read_from_scan(s, st, handle, &tuple));
+      }
+      /**
+       * Make sure the scan size.
+       */
+      EXPECT_EQ(Status::OK, scannable_total_index_size(s, st, handle, scan_size));
+      EXPECT_EQ(Status::OK, commit(s));
     }
-    /**
-     * Make sure the scan size.
-     */
-    ASSERT_EQ(Status::OK, scannable_total_index_size(s, st, handle, scan_size));
     cout << "scannable_total_index_size : " << scan_size << endl;
-    ASSERT_EQ(Status::OK, commit(s));
     end = rdtscp();
     perf_counter.stop();
-    cout << "Result : " << end - start << " [clocks]" << endl;
+    cout << "Result : " << (end - start) / loop << " [clocks]" << endl;
+    perf_counter /= loop;
     perf_counter.print();
   }
+}
+
+TEST_F(ScanPerfTest, read_from_scan) {
+  uint64_t key[MAX_TUPLES];
+  for (int i = 0; i < MAX_TUPLES; ++i) {
+    key[i] = i;
+  }
+  std::string v1("a");
+  EXPECT_EQ(Status::OK, enter(s));
+
+  for (int i = 0; i < 100; ++i) {
+    EXPECT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
+  }
+  EXPECT_EQ(Status::OK, commit(s));
+
+  cout << "Perform 100 records read_from_scan on a table with 100 records." << endl;
+  DoScan(10000);
 
   /**
    * Prepare for next experiments.
    */
   for (int i = 100; i < 1000; ++i) {
-    ASSERT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
+    EXPECT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
   }
-  ASSERT_EQ(Status::OK, commit(s));
+  EXPECT_EQ(Status::OK, commit(s));
 
   cout << "Perform 100 records read_from_scan on a table with 1K records." << endl;
-  {
-    PerfCounter perf_counter(true);
-    perf_counter.start();
-    start = rdtscp();
-    ASSERT_EQ(Status::OK, open_scan(s, st, nullptr, 0, false, nullptr, 0, false, handle));
-    for (int i = 0; i < 100; ++i) {
-      ASSERT_EQ(Status::OK, read_from_scan(s, st, handle, &tuple));
-    }
-    /**
-     * Make sure  the scan size.
-     */
-    ASSERT_EQ(Status::OK, scannable_total_index_size(s, st, handle, scan_size));
-    cout << "scannable_total_index_size : " << scan_size << endl;
-    ASSERT_EQ(Status::OK, commit(s));
-    end = rdtscp();
-    perf_counter.stop();
-    cout << "Result : " << end - start << " [clocks]" << endl;
-    perf_counter.print();
-  }
+  DoScan(10000);
 
   /**
    * Prepare for next experiments.
    */
   for (int i = 1000; i < 10000; ++i) {
-    ASSERT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
+    EXPECT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
   }
-  ASSERT_EQ(Status::OK, commit(s));
+  EXPECT_EQ(Status::OK, commit(s));
 
   cout << "Perform 100 records read_from_scan on a table with 10K records." << endl;
-  {
-    PerfCounter perf_counter(true);
-    perf_counter.start();
-    start = rdtscp();
-    ASSERT_EQ(Status::OK, open_scan(s, st, nullptr, 0, false, nullptr, 0, false, handle));
-    for (int i = 0; i < 100; ++i) {
-      ASSERT_EQ(Status::OK, read_from_scan(s, st, handle, &tuple));
-    }
-    /**
-     * Make sure  the scan size.
-     */
-    ASSERT_EQ(Status::OK, scannable_total_index_size(s, st, handle, scan_size));
-    cout << "scannable_total_index_size : " << scan_size << endl;
-    ASSERT_EQ(Status::OK, commit(s));
-    end = rdtscp();
-    perf_counter.stop();
-    cout << "Result : " << end - start << " [clocks]" << endl;
-    perf_counter.print();
-  }
+  DoScan(10000);
 
   /**
    * Prepare for next experiments.
    */
   for (int i = 10000; i < 20000; ++i) {
-    ASSERT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
+    EXPECT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
   }
-  ASSERT_EQ(Status::OK, commit(s));
+  EXPECT_EQ(Status::OK, commit(s));
 
   cout << "Perform 100 records read_from_scan on a table with 20K records." << endl;
-  {
-    PerfCounter perf_counter(true);
-    perf_counter.start();
-    start = rdtscp();
-    ASSERT_EQ(Status::OK, open_scan(s, st, nullptr, 0, false, nullptr, 0, false, handle));
-    for (int i = 0; i < 100; ++i) {
-      ASSERT_EQ(Status::OK, read_from_scan(s, st, handle, &tuple));
-    }
-    /**
-     * Make sure  the scan size.
-     */
-    ASSERT_EQ(Status::OK, scannable_total_index_size(s, st, handle, scan_size));
-    cout << "scannable_total_index_size : " << scan_size << endl;
-    ASSERT_EQ(Status::OK, commit(s));
-    end = rdtscp();
-    perf_counter.stop();
-    cout << "Result : " << end - start << " [clocks]" << endl;
-    perf_counter.print();
-  }
+  DoScan(10000);
 
   /**
    * Prepare for next experiments.
    */
   for (int i = 20000; i < 40000; ++i) {
-    ASSERT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
+    EXPECT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
   }
-  ASSERT_EQ(Status::OK, commit(s));
+  EXPECT_EQ(Status::OK, commit(s));
 
   cout << "Perform 100 records read_from_scan on a table with 40K records." << endl;
-  {
-    PerfCounter perf_counter(true);
-    perf_counter.start();
-    start = rdtscp();
-    ASSERT_EQ(Status::OK, open_scan(s, st, nullptr, 0, false, nullptr, 0, false, handle));
-    for (int i = 0; i < 100; ++i) {
-      ASSERT_EQ(Status::OK, read_from_scan(s, st, handle, &tuple));
-    }
-    /**
-     * Make sure  the scan size.
-     */
-    ASSERT_EQ(Status::OK, scannable_total_index_size(s, st, handle, scan_size));
-    cout << "scannable_total_index_size : " << scan_size << endl;
-    ASSERT_EQ(Status::OK, commit(s));
-    end = rdtscp();
-    perf_counter.stop();
-    cout << "Result : " << end - start << " [clocks]" << endl;
-    perf_counter.print();
-  }
+  DoScan(10000);
 
   /**
    * Prepare for next experiments.
    */
   for (int i = 40000; i < 80000; ++i) {
-    ASSERT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
+    EXPECT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
   }
-  ASSERT_EQ(Status::OK, commit(s));
+  EXPECT_EQ(Status::OK, commit(s));
 
   cout << "Perform 100 records read_from_scan on a table with 80K records." << endl;
-  {
-    PerfCounter perf_counter(true);
-    perf_counter.start();
-    start = rdtscp();
-    ASSERT_EQ(Status::OK, open_scan(s, st, nullptr, 0, false, nullptr, 0, false, handle));
-    for (int i = 0; i < 100; ++i) {
-      ASSERT_EQ(Status::OK, read_from_scan(s, st, handle, &tuple));
-    }
-    /**
-     * Make sure  the scan size.
-     */
-    ASSERT_EQ(Status::OK, scannable_total_index_size(s, st, handle, scan_size));
-    cout << "scannable_total_index_size : " << scan_size << endl;
-    ASSERT_EQ(Status::OK, commit(s));
-    end = rdtscp();
-    perf_counter.stop();
-    cout << "Result : " << end - start << " [clocks]" << endl;
-    perf_counter.print();
-  }
+  DoScan(1000);
 
   /**
    * Prepare for next experiments.
    */
   for (int i = 80000; i < 160000; ++i) {
-    ASSERT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
+    EXPECT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
   }
-  ASSERT_EQ(Status::OK, commit(s));
+  EXPECT_EQ(Status::OK, commit(s));
 
   cout << "Perform 100 records read_from_scan on a table with 160K records." << endl;
-  {
-    PerfCounter perf_counter(true);
-    perf_counter.start();
-    start = rdtscp();
-    ASSERT_EQ(Status::OK, open_scan(s, st, nullptr, 0, false, nullptr, 0, false, handle));
-    for (int i = 0; i < 100; ++i) {
-      ASSERT_EQ(Status::OK, read_from_scan(s, st, handle, &tuple));
-    }
-    /**
-     * Make sure  the scan size.
-     */
-    ASSERT_EQ(Status::OK, scannable_total_index_size(s, st, handle, scan_size));
-    cout << "scannable_total_index_size : " << scan_size << endl;
-    ASSERT_EQ(Status::OK, commit(s));
-    end = rdtscp();
-    perf_counter.stop();
-    cout << "Result : " << end - start << " [clocks]" << endl;
-    perf_counter.print();
-  }
+  DoScan(500);
 
   /**
    * Prepare for next experiments.
    */
   for (int i = 160000; i < 320000; ++i) {
-    ASSERT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
+    EXPECT_EQ(Status::OK, insert(s, st, (char *)&key[i], sizeof(key[i]), v1.data(), v1.size()));
   }
-  ASSERT_EQ(Status::OK, commit(s));
+  EXPECT_EQ(Status::OK, commit(s));
 
   cout << "Perform 100 records read_from_scan on a table with 320K records." << endl;
-  {
-    PerfCounter perf_counter(true);
-    perf_counter.start();
-    start = rdtscp();
-    ASSERT_EQ(Status::OK, open_scan(s, st, nullptr, 0, false, nullptr, 0, false, handle));
-    for (int i = 0; i < 100; ++i) {
-      ASSERT_EQ(Status::OK, read_from_scan(s, st, handle, &tuple));
-    }
-    /**
-     * Make sure  the scan size.
-     */
-    ASSERT_EQ(Status::OK, scannable_total_index_size(s, st, handle, scan_size));
-    cout << "scannable_total_index_size : " << scan_size << endl;
-    ASSERT_EQ(Status::OK, commit(s));
-    end = rdtscp();
-    perf_counter.stop();
-    cout << "Result : " << end - start << " [clocks]" << endl;
-    perf_counter.print();
-  }
+  DoScan(100);
 
-  ASSERT_EQ(Status::OK, leave(s));
+  EXPECT_EQ(Status::OK, leave(s));
 }
 
 } // namespace kvs_charkey::testing
