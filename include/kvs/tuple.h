af72cc15269147041eb85f901bbe8068af49a7f7
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2020-04-15 08:10:33 +0900

    improve pimpl idiom in tuple.

--- include/kvs/tuple.h
+++ include/kvs/tuple.h
@@ -13,6 +13,8 @@ namespace kvs {
 
 class Tuple {
 public:
+  class Impl;
+
   Tuple();
   Tuple (const char* key_ptr, const std::size_t key_length, const char* value_ptr, const std::size_t value_length);
   Tuple(const Tuple& right);
@@ -21,34 +23,11 @@ public:
   Tuple& operator=(Tuple&& right)&;
   ~Tuple();
 
-  std::string_view get_key()&;
   std::string_view get_key() const&;
-  std::string_view get_value()&;
   std::string_view get_value() const&;
-
-  void set(const char* key_ptr, const std::size_t key_length, const char* value_ptr, const std::size_t value_length)&;
-
-  /**
-   * @brief set key of data in local
-   * @details The memory area of old local data is overwritten..
-   */
-  void set_key(const char* key_ptr, const std::size_t key_length)&;
-
-  /**
-   * @brief set value of data in local
-   * @details The memory area of old local data is released immediately.
-   */
-  void set_value(const char* value_ptr, const std::size_t value_length)&;
-
-  /**
-   * @brief set value of data in global
-   * @details The memory area of old local data is managed by GabeColle.
-   * @params [out] old_value Tell the information to pass to GabeColle.
-   */
-  void set_value(const char* value_ptr, const std::size_t value_length, std::string** const old_value)&;
+  Impl* get_pimpl() &;
 
 private:
-  class Impl;
   std::unique_ptr<Impl> pimpl_;
 };
 
