fd5bee87db4907e77313c89f9ca8ab6786818210
tanabe takayuki <tanabe@hpcs.cs.tsukuba.ac.jp>
2020-04-16 08:38:31 +0900

    improve fin function.

--- src/include/gcollection.hh
+++ src/include/gcollection.hh
@@ -29,6 +29,21 @@ extern void delete_all_garbage_records();
  */
 extern void delete_all_garbage_values();
 
+/**
+ * @brief Release all heap objects in this system.
+ * @details Do three functions: delete_all_garbage_values(), delete_all_garbage_records(), and remove_all_leaf_from_mtdb_and_release().
+ * @pre This function should be called at terminating db.
+ * @return void
+ */
+extern void release_all_heap_objects();
+
+/**
+ * @brief Remove all leaf nodes from MTDB and release those heap objects.
+ * @pre This function should be called at terminating db.
+ * @return void
+ */
+extern void remove_all_leaf_from_mtdb_and_release();
+
 extern std::vector<Record*> kGarbageRecords[KVS_NUMBER_OF_LOGICAL_CORES];
 extern std::mutex kMutexGarbageRecords[KVS_NUMBER_OF_LOGICAL_CORES];
 extern std::vector<std::pair<std::string*, Epoch>> kGarbageValues[KVS_NUMBER_OF_LOGICAL_CORES];
