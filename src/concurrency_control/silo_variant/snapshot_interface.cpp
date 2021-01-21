3ab9261f69b1faf4991c89620e888997645f13e1
Takayuki Tanabe <thawk105@gmail.com>
2021-01-21 22:52:57 +0900

    fin impl of lookup_snapshot.

--- src/concurrency_control/silo_variant/snapshot_interface.cpp
+++ src/concurrency_control/silo_variant/snapshot_interface.cpp
@@ -12,7 +12,7 @@ using namespace cc_silo_variant;
 
 namespace shirakami::cc_silo_variant::snapshot_interface {
 
-Status lookup_snapshot(Token token, std::string_view key, Tuple** const ret_tuple) {
+Status lookup_snapshot(Token token, std::string_view key, Tuple** const ret_tuple) { // NOLINT
     auto* ti = static_cast<session_info*>(token);
 
     Record** rec_d_ptr{std::get<0>(yakushima::get<Record*>(key))};
@@ -23,26 +23,22 @@ Status lookup_snapshot(Token token, std::string_view key, Tuple** const ret_tupl
     }
 
     Record* rec_ptr{*rec_d_ptr};
-    tid_word tid = loadAcquire(rec_ptr->get_tidw().get_obj());
+    tid_word tid{};
 
-READ_PROCESS_START:
     // phase 1 : decide to see main record or snapshot.
-    // phase 1-1 : wait releasing lock
-    if (tid.get_lock()) {
-        do {
-            _mm_pause();
-            tid = loadAcquire(rec_ptr->get_tidw().get_obj());
-        } while (tid.get_lock());
-    }
+    for (;;) {
+        // phase 1-1 : wait releasing lock
+        for (tid = loadAcquire(rec_ptr->get_tidw().get_obj()); tid.get_lock(); _mm_pause(), tid = loadAcquire(
+                rec_ptr->get_tidw().get_obj())) {}
 
-    // phase 1-2 : check snapshot epoch
-    if (epoch::get_snap_epoch(ti->get_epoch()) != epoch::get_snap_epoch(tid.get_epoch())) {
-        // it should read from main record.
-    } else {
-        // it should read from snapshot.
-        if (rec_ptr->get_snap_ptr() != nullptr) {
-            // there is a snapshot to see.
-            Tuple escape_tuple = rec_ptr->get_snap_ptr()->get_tuple();
+        // phase 1-2 : check snapshot epoch
+        if (epoch::get_snap_epoch(ti->get_epoch()) > epoch::get_snap_epoch(tid.get_epoch())) {
+            /**
+             * it should read from main record.
+             * If it reads from main record, the read value may change after the reading due to the same memory address.
+             * So this case should escape the value to other memory address.
+             */
+            Tuple escape_tuple = rec_ptr->get_tuple();
             if (tid == loadAcquire(rec_ptr->get_tidw().get_obj())) {
                 // success atomic read
                 ti->get_read_only_tuples().emplace_back(std::move(escape_tuple));
@@ -50,30 +46,24 @@ READ_PROCESS_START:
                 return Status::OK;
             }
             // fail atomic read
-            goto READ_PROCESS_START; // NOLINT
-        } else {
-            // This record was inserted at same snap(epoch), so there is no snapshot.
-            return Status::WARN_NOT_FOUND;
+            continue;
+        }
+        break;
+    }
+    /**
+     * It should read from snapshot.
+     * This snap may be appropriate.
+     * This read does not need to be verified whether it could be read atomically from the list.
+     * Because the position that can be interrupted is the second, it is always from the main version,
+     * and this transaction does not have to read it.
+     */
+    for (Record* snap_ptr = rec_ptr->get_snap_ptr();; snap_ptr = snap_ptr->get_snap_ptr()) {
+        if (snap_ptr == nullptr) return Status::WARN_NOT_FOUND;
+        if (epoch::get_snap_epoch(ti->get_epoch()) > epoch::get_snap_epoch(snap_ptr->get_tidw().get_epoch())) {
+            *ret_tuple = &snap_ptr->get_tuple();
+            return Status::OK;
         }
     }
-
-#if 0
-    // todo : impl now
-    if (rec_ptr->get_snap_ptr() == nullptr) {
-        // case : There is no snapshot to see.
-        if (tid.get_lock()) {
-
-        } else {
-
-        }
-        if (epoch::get_snap_epoch(tid.epoch_))
-            return Status::WARN_NOT_FOUND;
-    } else {
-        // case : There is no snapshot to see.
-    }
-#endif
-
-    return Status::OK;
 }
 
 }
