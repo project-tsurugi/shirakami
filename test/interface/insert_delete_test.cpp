09f5663404ec5b72981e2a409beed0c42a4f0866
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2021-01-29 17:14:41 +0900

    test for updating primary key

--- test/interface/insert_delete_test.cpp
+++ test/interface/insert_delete_test.cpp
@@ -71,6 +71,38 @@ TEST_F(insert_delete, delete_insert) {  // NOLINT
     ASSERT_EQ(Status::OK, leave(s));
 }
 
+TEST_F(insert_delete, delete_insert_on_scan) {  // NOLINT
+  std::string k("testing");                       // NOLINT
+  std::string k2("new_key");                       // NOLINT
+  std::string v("bbb");                              // NOLINT
+  Token s{};
+  ASSERT_EQ(Status::OK, enter(s));
+  ASSERT_EQ(Status::OK, insert(s, k, v));
+  ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+  Tuple* t{};
+  ScanHandle handle;
+  ASSERT_EQ(Status::OK, open_scan(
+      s,
+      k, scan_endpoint::INCLUSIVE,
+      "", scan_endpoint::INF,
+      handle)
+  );
+  ASSERT_EQ(Status::OK, read_from_scan(
+      s,
+      handle,
+      &t)
+  );
+  ASSERT_TRUE(t); // NOLINT
+  ASSERT_EQ(Status::OK, delete_record(s, k));
+  ASSERT_EQ(Status::OK, insert(s, k2, v));
+  ASSERT_EQ(Status::OK, close_scan(s, handle));
+  ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+  ASSERT_EQ(Status::OK, search_key(s, k2, &t));
+  ASSERT_TRUE(t); // NOLINT
+  ASSERT_EQ(Status::WARN_NOT_FOUND, search_key(s, k, &t));
+  ASSERT_EQ(Status::OK, leave(s));
+}
+
 TEST_F(insert_delete, delete_upsert) {  // NOLINT
     std::string k("testing");                       // NOLINT
     std::string v("bbb");                              // NOLINT
