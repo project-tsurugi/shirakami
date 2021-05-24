38ac2a79dc1737a8b4c3b4cbb4db87a19d29d477
Takayuki Tanabe <thawk105@gmail.com>
2021-05-24 21:15:41 +0900

    fix test of loop count.

--- test/concurrency_control/delete/delete_after_delete_test.cpp
+++ test/concurrency_control/delete/delete_after_delete_test.cpp
@@ -23,33 +23,31 @@ TEST_F(delete_after_delete, delete_after_delete) { // NOLINT
     std::string k1("k");  // NOLINT
     std::string v1("v1"); // NOLINT
     Token s{};
-    for (std::size_t i = 0; i < 100; ++i) { // NOLINT
+    /**
+      * Test the interaction between snapshot manager and cpr manager by inserting enter / leave.
+      */
+    ASSERT_EQ(Status::OK, enter(s));
+    while (Status::WARN_NOT_FOUND == upsert(s, storage, k1, v1)) {
+        ;
         /**
-         * Test the interaction between snapshot manager and cpr manager by inserting enter / leave frequently.
-         */
-        ASSERT_EQ(Status::OK, enter(s));
-        while (Status::WARN_NOT_FOUND == upsert(s, storage, k1, v1)) {
-            ;
-            /**
-             * The last delete_record didn't unhook the record because of the snapshot manager or cpr manager, 
-             * so upsert is an update instruction because the record exists, and WARN_NOT_FOUND because it was a deleted record.
-             */
-            commit(s);
-            /**
-             * If you don't commit, the epoch and manager may not progress, so you may end up in an infinite loop here.
-             */
-        }
-        ASSERT_EQ(Status::OK, commit(s));
-        ASSERT_EQ(Status::OK, leave(s));
-        ASSERT_EQ(Status::OK, enter(s));
-        ASSERT_EQ(Status::OK, delete_record(s, storage, k1));
-        ASSERT_EQ(Status::OK, commit(s));
-        ASSERT_EQ(Status::OK, leave(s));
-        ASSERT_EQ(Status::OK, enter(s));
-        ASSERT_EQ(Status::WARN_NOT_FOUND, delete_record(s, storage, k1));
-        ASSERT_EQ(Status::OK, commit(s));
-        ASSERT_EQ(Status::OK, leave(s));
+          * The last delete_record didn't unhook the record because of the snapshot manager or cpr manager, 
+          * so upsert is an update instruction because the record exists, and WARN_NOT_FOUND because it was a deleted record.
+          */
+        commit(s);
+        /**
+          * If you don't commit, the epoch and manager may not progress, so you may end up in an infinite loop here.
+          */
     }
+    ASSERT_EQ(Status::OK, commit(s));
+    ASSERT_EQ(Status::OK, leave(s));
+    ASSERT_EQ(Status::OK, enter(s));
+    ASSERT_EQ(Status::OK, delete_record(s, storage, k1));
+    ASSERT_EQ(Status::OK, commit(s));
+    ASSERT_EQ(Status::OK, leave(s));
+    ASSERT_EQ(Status::OK, enter(s));
+    ASSERT_EQ(Status::WARN_NOT_FOUND, delete_record(s, storage, k1));
+    ASSERT_EQ(Status::OK, commit(s));
+    ASSERT_EQ(Status::OK, leave(s));
 }
 
 } // namespace shirakami::testing
