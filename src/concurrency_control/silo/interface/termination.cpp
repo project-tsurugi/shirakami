ab9a5309115b35d0d23a1bfa2674cb08a8dbbf72
Takayuki Tanabe <thawk105@gmail.com>
2021-10-28 15:07:53 +0900

    refactor pure silo commit phase.

--- src/concurrency_control/silo/interface/termination.cpp
+++ src/concurrency_control/silo/interface/termination.cpp
@@ -12,33 +12,40 @@
 
 namespace shirakami {
 
+void clean_up_session_flag(session* ti) {
+    ti->set_read_only(false);
+    ti->set_tx_began(false);
+}
+
+void clean_up_session_info(session* ti) {
+    ti->clean_up_local_set();
+    ti->clean_up_scan_caches();
+    clean_up_session_flag(ti);
+}
+
 Status abort(Token token) { // NOLINT
     auto* ti = static_cast<session*>(token);
 
     ti->get_write_set().remove_inserted_records_from_yakushima(
             token, ti->get_yakushima_token());
-    ti->clean_up_local_set();
-    ti->clean_up_scan_caches();
 
-    // about member
-    ti->set_read_only(false);
-    ti->set_tx_began(false);
+    clean_up_session_info(ti);
 
     return Status::OK;
 }
 
-extern Status commit(Token token, commit_param* cp) { // NOLINT
-    // Phase 1: Sort lock list;
+Status write_lock(Token token, tid_word& max_wset) {
     auto* ti = static_cast<session*>(token);
+
+    // Phase 1: Sort lock list;
     ti->get_write_set().sort_if_ol();
 
     // Phase 2: Lock write set;
-    tid_word max_rset;
-    tid_word max_wset;
     auto process = [token, ti, &max_wset](write_set_obj* we_ptr,
                                           std::size_t ctr) {
         // update/delete
         we_ptr->get_rec_ptr()->get_tidw().lock();
+        ++ctr;
         if ((we_ptr->get_op() == OP_TYPE::UPDATE ||
              we_ptr->get_op() == OP_TYPE::DELETE) && // NOLINT
             we_ptr->get_rec_ptr()->get_tidw().get_absent()) {
@@ -61,20 +68,34 @@ extern Status commit(Token token, commit_param* cp) { // NOLINT
         return Status::OK;
     };
 
-    std::size_t ctr{1};
+    std::size_t ctr{0};
     if (ti->get_write_set().get_for_batch()) {
         for (auto&& elem : ti->get_write_set().get_cont_for_bt()) {
             write_set_obj* we_ptr = &std::get<1>(elem);
-            if (we_ptr->get_op() != OP_TYPE::INSERT) { process(we_ptr, ctr); }
-            ++ctr;
+            if (we_ptr->get_op() != OP_TYPE::INSERT) {
+                auto rc = process(we_ptr, ctr);
+                if (rc != Status::OK) { return rc; }
+            }
         }
     } else {
         for (auto&& elem : ti->get_write_set().get_cont_for_ol()) {
             write_set_obj* we_ptr = &(elem);
-            if (we_ptr->get_op() != OP_TYPE::INSERT) { process(we_ptr, ctr); }
-            ++ctr;
+            if (we_ptr->get_op() != OP_TYPE::INSERT) {
+                auto rc = process(we_ptr, ctr);
+                if (rc != Status::OK) { return rc; }
+            }
         }
     }
+
+    return Status::OK;
+}
+
+extern Status commit(Token token, commit_param* cp) { // NOLINT
+    auto* ti = static_cast<session*>(token);
+
+    tid_word max_wset;
+    auto rc = write_lock(token, max_wset);
+    if (rc != Status::OK) { return rc; }
 
     // Serialization point
     asm volatile("" ::: "memory"); // NOLINT
@@ -90,6 +111,7 @@ extern Status commit(Token token, commit_param* cp) { // NOLINT
 
     // Phase 3: Validation
     tid_word check;
+    tid_word max_rset;
     for (auto&& itr : ti->get_read_set()) {
         const Record* rec_ptr = itr.get_rec_ptr();
         check.get_obj() = loadAcquire(rec_ptr->get_tidw().get_obj());
@@ -120,14 +142,6 @@ extern Status commit(Token token, commit_param* cp) { // NOLINT
     write_phase(ti, max_rset, max_wset,
                 cp != nullptr ? cp->get_cp()
                               : commit_property::NOWAIT_FOR_COMMIT);
-    /**
-     * about holding operation info.
-     */
-    ti->clean_up_local_set();
-    /**
-     * about scan operation.
-     */
-    ti->clean_up_scan_caches();
 
 #if defined(PWAL)
     if (cp != nullptr) cp->set_ctid(ti->get_mrctid().get_obj());
@@ -148,8 +162,8 @@ extern Status commit(Token token, commit_param* cp) { // NOLINT
     }
 #endif
 
-    ti->set_read_only(false);
-    ti->set_tx_began(false);
+    clean_up_session_info(ti);
+
     return Status::OK;
 }
 
