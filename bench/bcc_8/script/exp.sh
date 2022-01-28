f111676c838aa07e18c461e7c203aa4ce3e7717f
Takayuki Tanabe <thawk105@gmail.com>
2022-01-28 22:34:32 +0900

    fix bcc_8 script.

--- bench/bcc_8/script/exp.sh
+++ bench/bcc_8/script/exp.sh
@@ -3,117 +3,220 @@ set -e
 build() {
     echo "start build"
     cd ../../
-    rm -rf build_wp_release
-    mkdir build_wp_release
-    cd build_wp_release
-    # $1 is 1 for pure, 2 for r-anti-track
+    rm -rf build_cc_release
+    mkdir build_cc_release
+    cd build_cc_release
+    # $1 is 1 for vec, 2 for um
     if test $1 -eq 1; then
-        cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CPR=OFF -DBUILD_WP=ON ..
+        cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CPR=OFF -DPARAM_READ_SET_CONT=0 ..
     elif test $1 -eq 2; then
-        cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CPR=OFF -DBUILD_WP=ON -DBCC_7=ON ..
+        cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CPR=OFF -DPARAM_READ_SET_CONT=1 ..
     else
         echo "BAG $LINENO"
         exit
     fi
     cmake --build . --target all -- -j
     #return original position.
-    cd ../bench/bcc_7
+    cd ../bench/bcc_8
 }
 
 exp() {
     echo "start exp"
-    cd ../../build_wp_release/bench/bcc_7/
+    cd ../../build_cc_release/bench/bcc_8/
 
     duration=5
     epoch=5
-    result="bcc_7.dat"
+    skews=(0, 0.33, 0.66, 0.99)
+    skew=0
+    tx_sizes=(10 100 1000 10000)
+    tx_size=1000
+    result="bcc_8.dat"
     rm -f $result
 
     total_trial_num=0
-    for ((rec = 1000; rec <= 10000000; rec *= 10)); do
-        for ((i = 1; i <= epoch; ++i)); do
-            total_trial_num=$(echo "$total_trial_num + 1" | bc)
+    if test $1 -eq 1; then
+        for tx_size in "${tx_sizes[@]}"; do
+            for ((i = 1; i <= epoch; ++i)); do
+                total_trial_num=$(echo "$total_trial_num + 1" | bc)
+            done
         done
-    done
+    elif test $1 -eq 2; then
+        for skew in "${skews[@]}"; do
+            for ((i = 1; i <= epoch; ++i)); do
+                total_trial_num=$(echo "$total_trial_num + 1" | bc)
+            done
+        done
+    else
+        echo "BAG $LINENO"
+        exit
+    fi
+
     echo "total_trial_num is $total_trial_num"
 
     trial_num=0
-    for ((rec = 1000; rec <= 10000000; rec *= 10)); do
-        echo "#sudo perf stat -e cache-misses,cache-references -o ana.txt numactl --interleave=all ./bcc_7 -d $duration -rec $rec"
+    if test $1 -eq 1; then
+        for tx_size in "${tx_sizes[@]}"; do
+            echo "#sudo perf stat -e cache-misses,cache-references -o ana.txt numactl --interleave=all ./bcc_8 -d $duration -skew $skew -tx_size $tx_size"
 
-        sum_th=0
-        max_th=0
-        min_th=0
+            sum_th=0
+            sum_mm=0
+            max_th=0
+            max_mm=0
+            min_th=0
+            min_mm=0
 
-        for ((i = 1; i <= epoch; ++i)); do
-            trial_num=$(echo "$trial_num + 1" | bc)
-            rate_to_all=$(echo "scale=4; $trial_num / $total_trial_num" | bc | xargs printf %.4f)
-            echo "rate_to_all is $rate_to_all"
-            perc_rate_to_all=$(echo "$rate_to_all * 100" | bc | xargs printf %2.4f)
-            echo "perc_rate_to_all is $perc_rate_to_all"
+            for ((i = 1; i <= epoch; ++i)); do
+                trial_num=$(echo "$trial_num + 1" | bc)
+                rate_to_all=$(echo "scale=4; $trial_num / $total_trial_num" | bc | xargs printf %.4f)
+                echo "rate_to_all is $rate_to_all"
+                perc_rate_to_all=$(echo "$rate_to_all * 100" | bc | xargs printf %2.4f)
+                echo "perc_rate_to_all is $perc_rate_to_all"
 
-            date
-            sudo perf stat -e cache-misses,cache-references -o ana.txt numactl --interleave=all ./bcc_7 -d $duration -rec $rec >exp.txt
-            tmp_th=$(grep "throughput" ./exp.txt | awk '{print $2}')
-            sum_th=$(echo "scale=4; $sum_th + $tmp_th" | bc | xargs printf %10.4f)
-            echo "tmp_th: $tmp_th"
+                date
+                sudo perf stat -e cache-misses,cache-references -o ana.txt numactl --interleave=all ./bcc_8 -d $duration -skew $skew -tx_size $tx_size >exp.txt
+                tmp_th=$(grep "throughput" ./exp.txt | awk '{print $2}')
+                tmp_mm=$(grep "maxrss" ./exp.txt | awk '{print $2}')
+                sum_th=$(echo "scale=4; $sum_th + $tmp_th" | bc | xargs printf %10.4f)
+                sum_mm=$(echo "scale=4; $sum_mm + $tmp_mm" | bc | xargs printf %10.4f)
+                echo "tmp_th: $tmp_th, tmp_mm: $tmp_mm"
 
-            if test $i -eq 1; then
-                max_th=$tmp_th
-                min_th=$tmp_th
-            fi
+                if test $i -eq 1; then
+                    max_th=$tmp_th
+                    min_th=$tmp_th
+                    max_mm=$tmp_mm
+                    min_mm=$tmp_mm
+                fi
 
-            flag=$(echo "$tmp_th > $max_th" | bc)
-            if test $flag -eq 1; then
-                max_th=$tmp_th
-            fi
+                flag=$(echo "$tmp_th > $max_th" | bc)
+                if test $flag -eq 1; then
+                    max_th=$tmp_th
+                fi
 
-            flag=$(echo "$tmp_th < $min_th" | bc)
-            if test $flag -eq 1; then
-                min_th=$tmp_th
-            fi
+                flag=$(echo "$tmp_th < $min_th" | bc)
+                if test $flag -eq 1; then
+                    min_th=$tmp_th
+                fi
 
+                flag=$(echo "$tmp_mm > $max_mm" | bc)
+                if test $flag -eq 1; then
+                    max_mm=$tmp_mm
+                fi
+
+                flag=$(echo "$tmp_mm < $min_mm" | bc)
+                if test $flag -eq 1; then
+                    min_mm=$tmp_mm
+                fi
+            done
+            avg_th=$(echo "scale=4; $sum_th / $epoch" | bc | xargs printf %10.4f)
+            avg_mm=$(echo "scale=4; $sum_mm / $epoch" | bc | xargs printf %10.4f)
+            echo "$tx_size $avg_th $min_th $max_th $avg_mm $min_mm $max_mm" >>$result
         done
-        avg_th=$(echo "scale=4; $sum_th / $epoch" | bc | xargs printf %10.4f)
-        echo "$rec $avg_th $min_th $max_th" >>$result
-    done
+    elif test $1 -eq 2; then
+        for skew in "${skews[@]}"; do
+            echo "#sudo perf stat -e cache-misses,cache-references -o ana.txt numactl --interleave=all ./bcc_8 -d $duration -skew $skew -tx_size $tx_size"
+
+            sum_th=0
+            sum_mm=0
+            max_th=0
+            max_mm=0
+            min_th=0
+            min_mm=0
+
+            for ((i = 1; i <= epoch; ++i)); do
+                trial_num=$(echo "$trial_num + 1" | bc)
+                rate_to_all=$(echo "scale=4; $trial_num / $total_trial_num" | bc | xargs printf %.4f)
+                echo "rate_to_all is $rate_to_all"
+                perc_rate_to_all=$(echo "$rate_to_all * 100" | bc | xargs printf %2.4f)
+                echo "perc_rate_to_all is $perc_rate_to_all"
+
+                date
+                sudo perf stat -e cache-misses,cache-references -o ana.txt numactl --interleave=all ./bcc_8 -d $duration -skew $skew -tx_size $tx_size >exp.txt
+                tmp_th=$(grep "throughput" ./exp.txt | awk '{print $2}')
+                tmp_mm=$(grep "maxrss" ./exp.txt | awk '{print $2}')
+                sum_th=$(echo "scale=4; $sum_th + $tmp_th" | bc | xargs printf %10.4f)
+                sum_mm=$(echo "scale=4; $sum_mm + $tmp_mm" | bc | xargs printf %10.4f)
+                echo "tmp_th: $tmp_th, tmp_mm: $tmp_mm"
+
+                if test $i -eq 1; then
+                    max_th=$tmp_th
+                    min_th=$tmp_th
+                    max_mm=$tmp_mm
+                    min_mm=$tmp_mm
+                fi
+
+                flag=$(echo "$tmp_th > $max_th" | bc)
+                if test $flag -eq 1; then
+                    max_th=$tmp_th
+                fi
+
+                flag=$(echo "$tmp_th < $min_th" | bc)
+                if test $flag -eq 1; then
+                    min_th=$tmp_th
+                fi
+
+                flag=$(echo "$tmp_mm > $max_mm" | bc)
+                if test $flag -eq 1; then
+                    max_mm=$tmp_mm
+                fi
+
+                flag=$(echo "$tmp_mm < $min_mm" | bc)
+                if test $flag -eq 1; then
+                    min_mm=$tmp_mm
+                fi
+            done
+            avg_th=$(echo "scale=4; $sum_th / $epoch" | bc | xargs printf %10.4f)
+            avg_mm=$(echo "scale=4; $sum_mm / $epoch" | bc | xargs printf %10.4f)
+            echo "$skew $avg_th $min_th $max_th $avg_mm $min_mm $max_mm" >>$result
+        done
+    else
+        echo "BAG $LINENO"
+        exit
+    fi
 
     #return original pos
-    cd ../../../bench/bcc_7
+    cd ../../../bench/bcc_8
 }
 
 gen_graph() {
-    # $1 is 1 for pure, 2 for r-anti-track
+    # $1 is 1 for vector, 2 for unordered_map
     echo "start gen_graph"
-    cd ../../build_wp_release/bench/bcc_7
+    cd ../../build_cc_release/bench/bcc_8
     if test $1 -eq 1; then
-        gnuplot -e 'tname="pure"' ./../../../bench/bcc_7/script/graph.plt
+        gnuplot -e 'tname="vector - tx size"' ./../../../bench/bcc_8/script/graph.plt
     elif test $1 -eq 2; then
-        gnuplot -e 'tname="ra"' ./../../../bench/bcc_7/script/graph.plt
+        gnuplot -e 'tname="vector - skew"' ./../../../bench/bcc_8/script/graph.plt
+    elif test $1 -eq 3; then
+        gnuplot -e 'tname="unordered_map - tx size"' ./../../../bench/bcc_8/script/graph.plt
+    elif test $1 -eq 4; then
+        gnuplot -e 'tname="unordered_map - skew"' ./../../../bench/bcc_8/script/graph.plt
     else
         echo "BAG $LINENO"
         exit
     fi
 
     #escap pdf
-    cp bcc_7*pdf /tmp/
+    cp bcc_8*pdf /tmp/
 
     #return original pos
-    cd ../../../bench/bcc_7
+    cd ../../../bench/bcc_8
 }
 
 main() {
     echo "start main"
     # cleanup escaped pdf
-    rm -f /tmp/bcc_7*pdf
+    rm -f /tmp/bcc_8*pdf
     build 1
-    exp
+    exp 1
     gen_graph 1
-    build 2
-    exp
+    exp 2
     gen_graph 2
+    build 2
+    exp 1
+    gen_graph 3
+    exp 2
+    gen_graph 4
     ## return escaped pdf
-    cp /tmp/bcc_7*pdf ./../../build_wp_release/bench/bcc_7/
+    cp /tmp/bcc_8*pdf ./../../build_cc_release/bench/bcc_8/
 }
 
 main
--- bench/bcc_8/script/graph.plt
+++ bench/bcc_8/script/graph.plt
@@ -1,23 +1,52 @@
-set xlabel "# recs"
-set logscale x
-set format x "%1.1t{/Symbol \264}10^{%T}"
 set format y "%1.1t{/Symbol \264}10^{%T}"
+set format y2 "%1.1t{/Symbol \264}10^{%T}"
 
 set grid
 
 set terminal pdfcairo enhanced color
 
 set ylabel "throughput[tps]"
+set y2label "maxrss[kb]"
 if (exist("tname")) {
        set title tname
-       if (tname eq "pure") {
-              set output "bcc_7_pure.pdf"
-       } else {
-              set output "bcc_7_ra.pdf"
+       if (tname eq "vector - tx size") {
+              set output "bcc_8_vec_txs.pdf"
+              set xlabel "tx size"
+              set logscale x
+              set format x "%1.1t{/Symbol \264}10^{%T}"
+              set autoscale x
+              plot \
+              "bcc_8.dat"   u 1:2:3:4 w errorlines axes x1y1 title "throughput", \
+              "bcc_8.dat"   u 1:5:6:7 w errorlines axes x1y2 title "maxrss", \
+       } else if (tname eq "vector - skew") {
+              set output "bcc_8_vec_skew.pdf"
+              set xlabel "skew"
+              set nologscale x
+              set format x "%3.2f"
+              set xrange [0:1]
+              plot \
+              "bcc_8.dat"   u 1:2:3:4 w errorlines axes x1y1 title "throughput", \
+              "bcc_8.dat"   u 1:5:6:7 w errorlines axes x1y2 title "maxrss", \
+       } else if (tname eq "unordered_map - tx size") {
+              set output "bcc_8_um_txs.pdf"
+              set xlabel "tx size"
+              set logscale x
+              set format x "%1.1t{/Symbol \264}10^{%T}"
+              set autoscale x
+              plot \
+              "bcc_8.dat"   u 1:2:3:4 w errorlines axes x1y1 title "throughput", \
+              "bcc_8.dat"   u 1:5:6:7 w errorlines axes x1y2 title "maxrss", \
+       } else if (tname eq "unordered_map - skew") {
+              set output "bcc_8_um_skew.pdf"
+              set xlabel "skew"
+              set nologscale x
+              set format x "%3.2f"
+              set xrange [0:1]
+              plot \
+              "bcc_8.dat"   u 1:2:3:4 w errorlines axes x1y1 title "throughput", \
+              "bcc_8.dat"   u 1:5:6:7 w errorlines axes x1y2 title "maxrss", \
        }
 }
 
-plot \
-       "bcc_7.dat"   using 1:2:3:4 w errorlines notitle, \
 
 
