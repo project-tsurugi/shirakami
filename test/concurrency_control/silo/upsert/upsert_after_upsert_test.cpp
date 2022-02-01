e20953ca498b4b55bbb2991bb078cd50d9183a97
Takayuki Tanabe <thawk105@gmail.com>
2022-02-01 12:36:54 +0900

    add new test.

--- test/concurrency_control/silo/upsert/upsert_after_upsert_test.cpp
+++ test/concurrency_control/silo/upsert/upsert_after_upsert_test.cpp
@@ -21,14 +21,14 @@ namespace shirakami::testing {
 using namespace shirakami;
 
 Storage st{};
-class simple_upsert : public ::testing::Test { // NOLINT
+class upsert_after_upsert : public ::testing::Test { // NOLINT
 public:
     static void call_once_f() {
         google::InitGoogleLogging("shirakami-test-concurrency_control-silo-"
-                                  "upsert-simple_upsert_test");
+                                  "upsert-upsert_after_upsert_test");
         FLAGS_stderrthreshold = 0;        // output more than INFO
         log_dir_ = MAC2STR(PROJECT_ROOT); // NOLINT
-        log_dir_.append("/tmp/simple_upsert_test_log");
+        log_dir_.append("/tmp/upsert_after_upsert_test_log");
     }
     void SetUp() override {
         std::call_once(init_google_, call_once_f);
@@ -46,34 +46,43 @@ private:
     static inline std::string log_dir_;        // NOLINT
 };
 
-TEST_F(simple_upsert, upsert) { // NOLINT
-    std::string k("aaa");       // NOLINT
-    std::string v("aaa");       // NOLINT
-    std::string v2("bbb");      // NOLINT
+TEST_F(upsert_after_upsert, double_upsert) { // NOLINT
+    std::string k("aaa");                    // NOLINT
+    std::string v("bbb");                    // NOLINT
+    std::string v2("ccc");                   // NOLINT
+    std::string v3("ddd");                   // NOLINT
     Token s{};
-    Tuple* tuple{};
     ASSERT_EQ(Status::OK, enter(s));
     ASSERT_EQ(Status::OK, upsert(s, st, k, v));
     ASSERT_EQ(Status::OK, commit(s));
-    ASSERT_EQ(Status::WARN_ALREADY_EXISTS, insert(s, st, k, v));
-    ASSERT_EQ(Status::OK, commit(s));
+    Tuple* tuple{};
     ASSERT_EQ(Status::OK, upsert(s, st, k, v2));
-    ASSERT_EQ(Status::OK, commit(s));
-    ASSERT_EQ(Status::OK, search_key(s, st, k, tuple));
-    ASSERT_EQ(memcmp(tuple->get_value().data(), v2.data(), v2.size()), 0);
+    ASSERT_EQ(Status::WARN_WRITE_TO_LOCAL_WRITE, upsert(s, st, k, v3));
+    ASSERT_EQ(Status::WARN_READ_FROM_OWN_OPERATION,
+              search_key(s, st, k, tuple));
+    ASSERT_EQ(memcmp(tuple->get_value().data(), v3.data(), v3.size()), 0);
     ASSERT_EQ(Status::OK, commit(s));
     ASSERT_EQ(Status::OK, leave(s));
 }
 
-TEST_F(simple_upsert, upsert_after_insert) { // NOLINT
+TEST_F(upsert_after_upsert, manyUpsert) { // NOLINT
     std::string k("K");
-    std::string v("v");
+    std::string v(1000, 'v'); // NOLINT
     Token s{};
+    Token s2{};
     ASSERT_EQ(Status::OK, enter(s));
-    ASSERT_EQ(Status::OK, insert(s, st, k, v));
-    ASSERT_EQ(Status::WARN_WRITE_TO_LOCAL_WRITE, upsert(s, st, k, v));
-    ASSERT_EQ(Status::OK, commit(s));
+    ASSERT_EQ(Status::OK, enter(s2));
+
+    ASSERT_EQ(tx_begin(s2), Status::OK); // to block gc
+
+    for (std::size_t i = 0; i < 1000; ++i) { // NOLINT
+        ASSERT_EQ(Status::OK, upsert(s, st, k, v));
+        ASSERT_EQ(Status::OK, commit(s));
+    }
+
     ASSERT_EQ(Status::OK, leave(s));
+    ASSERT_EQ(Status::OK, leave(s2));
+    LOG(INFO) << "maxrss:\t" << getRusageRUMaxrss();
 }
 
 } // namespace shirakami::testing
