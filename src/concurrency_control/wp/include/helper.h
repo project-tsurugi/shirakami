9547349894ff0e57a5f510056c052421df66acd0
Takayuki Tanabe <thawk105@gmail.com>
2022-02-15 17:20:05 +0900

    fix for doxygen

--- src/concurrency_control/wp/include/helper.h
+++ src/concurrency_control/wp/include/helper.h
@@ -10,14 +10,6 @@
 
 namespace shirakami {
 
-/**
- * @brief Read record by using arguments infomation. It is used for optimistic
- * read.
- * @param[in] rec_ptr Target of read
- * @param[out] tid The result timestamp of read. It will be used for validation.
- * @param[out] val The result value of read.
- * @param[in] read_value whether read the value.
- */
 Status read_record(Record* rec_ptr, tid_word& tid, std::string& val, bool read_value);
 
 } // namespace shirakami
