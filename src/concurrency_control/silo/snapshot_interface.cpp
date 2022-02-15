2d0e4ed363e5ff8aef417e2b75fc4443c5d4d461
Takayuki Tanabe <thawk105@gmail.com>
2022-02-15 16:57:13 +0900

    refactor snapshot tx

--- src/concurrency_control/silo/snapshot_interface.cpp
+++ src/concurrency_control/silo/snapshot_interface.cpp
@@ -112,12 +112,7 @@ extern Status read_value_from_scan([[maybe_unused]] session* ti,
 
     auto itr = scan_buf.begin() + scan_index;
     Record* rec_ptr{const_cast<Record*>(std::get<0>(*itr))};
-    std::string out{};
-    auto rc{read_record(ti, rec_ptr, out)};
-    if (rc == Status::OK || rc == Status::WARN_CONCURRENT_INSERT) {
-        if (rc == Status::OK) { value = out; }
-    }
-    return rc;
+    return read_record(ti, rec_ptr, value);
 }
 
 extern Status read_record(session* const ti, Record* const rec_ptr,
@@ -141,16 +136,9 @@ extern Status read_record(session* const ti, Record* const rec_ptr,
              * So this case should escape the value to other memory address.
              */
             if (tid.get_absent()) return Status::WARN_NOT_FOUND;
-            std::string read_value_st;
-            if (read_value) { rec_ptr->get_tuple().get_value(read_value_st); }
+            if (read_value) { rec_ptr->get_tuple().get_value(value); }
             if (tid == loadAcquire(rec_ptr->get_tidw().get_obj())) {
                 // success atomic read
-                if (read_value) {
-                    std::string key{};
-                    rec_ptr->get_key(key);
-                    ti->get_read_only_tuples().emplace_back(key, read_value_st);
-                    ti->get_read_only_tuples().back().get_value(value);
-                }
                 return Status::OK;
             }
             // fail atomic read
@@ -178,81 +166,81 @@ extern Status read_record(session* const ti, Record* const rec_ptr,
     return Status::WARN_NOT_FOUND; // snap_ptr == nullptr
 }
 
-Status scan_key(session* ti, Storage storage, const std::string_view l_key,
-                const scan_endpoint l_end, // NOLINT
-                const std::string_view r_key, const scan_endpoint r_end,
-                std::vector<const Tuple*>& result, std::size_t max_size) {
-    // as a precaution
-    result.clear();
-
-    // optimize for small memory
-    ti->get_read_only_tuples().clear();
-
-    std::vector<std::tuple<std::string, Record**, std::size_t>> scan_buf;
-    constexpr std::size_t scan_buf_rec_ptr{1};
-    yakushima::scan(
-            {reinterpret_cast<char*>(&storage), sizeof(storage)}, // NOLINT
-            l_key, parse_scan_endpoint(l_end), r_key,
-            parse_scan_endpoint(r_end), scan_buf, nullptr, max_size);
-
-    if (scan_buf.empty()) return Status::WARN_NOT_FOUND;
-    for (auto&& elem : scan_buf) {
-        Record* rec_ptr = *std::get<scan_buf_rec_ptr>(elem);
-        tid_word tid{};
-        // phase 1 : decide to see main record or snapshot.
-        bool cont_next_scan_buf{false};
-        for (;;) {
-            // phase 1-1 : wait releasing lock
-            for (tid = loadAcquire(rec_ptr->get_tidw().get_obj());
-                 tid.get_lock(); _mm_pause(),
-                tid = loadAcquire(rec_ptr->get_tidw().get_obj())) {}
-
-            // phase 1-2 : check snapshot epoch
-            if (snapshot_manager::get_snap_epoch(ti->get_epoch()) >
-                snapshot_manager::get_snap_epoch(tid.get_epoch())) {
-                /**
-                 * it should read from main record.
-                 * If it reads from main record, the read value may change after the reading due to the same memory address.
-                 * So this case should escape the value to other memory address.
-                 */
-                if (tid.get_absent()) {
-                    cont_next_scan_buf = true;
-                    break;
-                }
-                Tuple escape_tuple = rec_ptr->get_tuple();
-                if (tid == loadAcquire(rec_ptr->get_tidw().get_obj())) {
-                    // success atomic read
-                    ti->get_read_only_tuples().emplace_back(
-                            std::move(escape_tuple));
-                    result.emplace_back(&ti->get_read_only_tuples().back());
-                    cont_next_scan_buf = true;
-                    break;
-                }
-                // fail atomic read
-                continue;
-            }
-            break;
-        }
-        if (cont_next_scan_buf) continue;
-        /**
-         * It should read from snapshot.
-         * Note that snapshot must not be absent (deleted) record.
-         * This snap may be appropriate.
-         * This read does not need to be verified whether it could be read atomically from the list.
-         * Because the position that can be interrupted is the second, it is always from the main version,
-         * and this transaction does not have to read it.
-         */
-        for (Record* snap_ptr = rec_ptr->get_snap_ptr(); snap_ptr != nullptr;
-             snap_ptr = snap_ptr->get_snap_ptr()) {
-            if (snapshot_manager::get_snap_epoch(ti->get_epoch()) >
-                snapshot_manager::get_snap_epoch(
-                        snap_ptr->get_tidw().get_epoch())) {
-                result.emplace_back(&snap_ptr->get_tuple());
-                break;
-            }
-        }
-    }
-    return Status::OK;
-}
+//Status scan_key(session* ti, Storage storage, const std::string_view l_key,
+//                const scan_endpoint l_end, // NOLINT
+//                const std::string_view r_key, const scan_endpoint r_end,
+//                std::vector<const Tuple*>& result, std::size_t max_size) {
+//    // as a precaution
+//    result.clear();
+//
+//    // optimize for small memory
+//    ti->get_read_only_tuples().clear();
+//
+//    std::vector<std::tuple<std::string, Record**, std::size_t>> scan_buf;
+//    constexpr std::size_t scan_buf_rec_ptr{1};
+//    yakushima::scan(
+//            {reinterpret_cast<char*>(&storage), sizeof(storage)}, // NOLINT
+//            l_key, parse_scan_endpoint(l_end), r_key,
+//            parse_scan_endpoint(r_end), scan_buf, nullptr, max_size);
+//
+//    if (scan_buf.empty()) return Status::WARN_NOT_FOUND;
+//    for (auto&& elem : scan_buf) {
+//        Record* rec_ptr = *std::get<scan_buf_rec_ptr>(elem);
+//        tid_word tid{};
+//        // phase 1 : decide to see main record or snapshot.
+//        bool cont_next_scan_buf{false};
+//        for (;;) {
+//            // phase 1-1 : wait releasing lock
+//            for (tid = loadAcquire(rec_ptr->get_tidw().get_obj());
+//                 tid.get_lock(); _mm_pause(),
+//                tid = loadAcquire(rec_ptr->get_tidw().get_obj())) {}
+//
+//            // phase 1-2 : check snapshot epoch
+//            if (snapshot_manager::get_snap_epoch(ti->get_epoch()) >
+//                snapshot_manager::get_snap_epoch(tid.get_epoch())) {
+//                /**
+//                 * it should read from main record.
+//                 * If it reads from main record, the read value may change after the reading due to the same memory address.
+//                 * So this case should escape the value to other memory address.
+//                 */
+//                if (tid.get_absent()) {
+//                    cont_next_scan_buf = true;
+//                    break;
+//                }
+//                Tuple escape_tuple = rec_ptr->get_tuple();
+//                if (tid == loadAcquire(rec_ptr->get_tidw().get_obj())) {
+//                    // success atomic read
+//                    ti->get_read_only_tuples().emplace_back(
+//                            std::move(escape_tuple));
+//                    result.emplace_back(&ti->get_read_only_tuples().back());
+//                    cont_next_scan_buf = true;
+//                    break;
+//                }
+//                // fail atomic read
+//                continue;
+//            }
+//            break;
+//        }
+//        if (cont_next_scan_buf) continue;
+//        /**
+//         * It should read from snapshot.
+//         * Note that snapshot must not be absent (deleted) record.
+//         * This snap may be appropriate.
+//         * This read does not need to be verified whether it could be read atomically from the list.
+//         * Because the position that can be interrupted is the second, it is always from the main version,
+//         * and this transaction does not have to read it.
+//         */
+//        for (Record* snap_ptr = rec_ptr->get_snap_ptr(); snap_ptr != nullptr;
+//             snap_ptr = snap_ptr->get_snap_ptr()) {
+//            if (snapshot_manager::get_snap_epoch(ti->get_epoch()) >
+//                snapshot_manager::get_snap_epoch(
+//                        snap_ptr->get_tidw().get_epoch())) {
+//                result.emplace_back(&snap_ptr->get_tuple());
+//                break;
+//            }
+//        }
+//    }
+//    return Status::OK;
+//}
 
 } // namespace shirakami::snapshot_interface
