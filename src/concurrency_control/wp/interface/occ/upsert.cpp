00992a0878e479b2daf3dc97493ed686886ec9e3
Takayuki Tanabe <thawk105@gmail.com>
2022-02-18 09:49:30 +0900

    remove goto label from wp::occ::upsert

--- src/concurrency_control/wp/interface/occ/upsert.cpp
+++ src/concurrency_control/wp/interface/occ/upsert.cpp
@@ -44,35 +44,30 @@ Status upsert(session* ti, Storage storage, const std::string_view key,
         return Status::WARN_INVALID_HANDLE;
     }
 
-RETRY_INDEX_ACCESS:
-
-    // index access to check local write set
-    Record* rec_ptr{};
-    if (Status::OK == get<Record>(storage, key, rec_ptr)) {
-        // check local write
-        write_set_obj* in_ws{ti->get_write_set().search(rec_ptr)}; // NOLINT
-        if (in_ws != nullptr) {
-            if (in_ws->get_op() == OP_TYPE::INSERT) {
-                in_ws->get_rec_ptr()->get_latest()->set_value(val);
-            } else {
-                in_ws->set_op(OP_TYPE::UPSERT);
-                in_ws->set_val(val);
+    for (;;) {
+        // index access to check local write set
+        Record* rec_ptr{};
+        if (Status::OK == get<Record>(storage, key, rec_ptr)) {
+            // check local write
+            write_set_obj* in_ws{ti->get_write_set().search(rec_ptr)}; // NOLINT
+            if (in_ws != nullptr) {
+                if (in_ws->get_op() == OP_TYPE::INSERT) {
+                    in_ws->get_rec_ptr()->get_latest()->set_value(val);
+                } else {
+                    in_ws->set_op(OP_TYPE::UPSERT);
+                    in_ws->set_val(val);
+                }
+                return Status::WARN_WRITE_TO_LOCAL_WRITE;
             }
-            return Status::WARN_WRITE_TO_LOCAL_WRITE;
+
+            // prepare write
+            ti->get_write_set().push(
+                    {storage, OP_TYPE::UPSERT, rec_ptr, val}); // NOLINT
+            return Status::OK;
         }
 
-        // prepare write
-        ti->get_write_set().push(
-                {storage, OP_TYPE::UPSERT, rec_ptr, val}); // NOLINT
-        return Status::OK;
-    }
-
-    auto rc{insert_process(ti, storage, key, val)};
-    if (rc == Status::WARN_CONCURRENT_INSERT) {
-        goto RETRY_INDEX_ACCESS; // NOLINT
-
-    } else {
-        return rc;
+        auto rc{insert_process(ti, storage, key, val)};
+        if (rc != Status::WARN_CONCURRENT_INSERT) { return rc; }
     }
 }
 
