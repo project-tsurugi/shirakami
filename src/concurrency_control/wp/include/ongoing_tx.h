b83cfa41925b34ea712b3ada56b6ef8c9dac6be6
Takayuki Tanabe <thawk105@gmail.com>
2022-03-08 09:42:43 +0900

    update about ongoing_tx's impl and test

--- src/concurrency_control/wp/include/ongoing_tx.h
+++ src/concurrency_control/wp/include/ongoing_tx.h
@@ -59,23 +59,27 @@ public:
         bool first{true};
         bool erased{false};
         for (auto it = tx_info_.begin(); it != tx_info_.end();) {
-            // update lowest epoch
-            if (first) {
-                lep = (*it).first;
-                first = false;
-            } else {
-                if ((*it).first < lep) { lep = (*it).first; }
-            }
-
             if (!erased && (*it).second == id) {
                 tx_info_.erase(it);
                 erased = true;
             } else {
+                // update lowest epoch
+                if (first) {
+                    lep = (*it).first;
+                    first = false;
+                } else {
+                    if ((*it).first < lep) { lep = (*it).first; }
+                }
+
                 ++it;
             }
         }
-        set_lowest_epoch(lep);
-        if (!erased) { LOG(FATAL); }
+        if (!tx_info_.empty()) {
+            set_lowest_epoch(lep);
+        } else {
+            set_lowest_epoch(0);
+        }
+        if (!erased) { LOG(ERROR) << "programming error."; }
     }
 
     static void set_lowest_epoch(epoch::epoch_t ep) {
@@ -83,8 +87,19 @@ public:
     }
 
 private:
-    static inline std::mutex mtx_;                        // NOLINT
-    static inline tx_info_type tx_info_;                          // NOLINT
+    /**
+     * @brief This is mutex for tx_info_;
+     */
+    static inline std::mutex mtx_; // NOLINT
+    /**
+     * @brief register info of running long tx's epoch and id.
+     */
+    static inline tx_info_type tx_info_; // NOLINT
+    /**
+     * @brief lowest epoch of running long tx
+     * @details This variables is read by short tx and long tx both. This is 
+     * used for read_by gc.
+     */
     static inline std::atomic<epoch::epoch_t> lowest_epoch_{0}; // NOLINT
 };
 
--- test/concurrency_control/wp/ongoing_tx_test.cpp
+++ test/concurrency_control/wp/ongoing_tx_test.cpp
@@ -1,5 +1,5 @@
 
-#include <glog/logging.h>
+#include <mutex>
 
 #include "concurrency_control/wp/include/ongoing_tx.h"
 
@@ -7,22 +7,81 @@
 
 #include "gtest/gtest.h"
 
+#include "glog/logging.h"
+
 using namespace shirakami;
 
 namespace shirakami::testing {
 
 class ongoing_tx_test : public ::testing::Test { // NOLINT
 public:
-    void SetUp() override { init(); } // NOLINT
+    static void call_once_f() {
+        google::InitGoogleLogging("shirakami-test-concurrency_control-wp-"
+                                  "ongoing_tx_test");
+        FLAGS_stderrthreshold = 0;        // output more than INFO
+        log_dir_ = MAC2STR(PROJECT_ROOT); // NOLINT
+        log_dir_.append("/tmp/ongoing_tx_test_log");
+    }
+
+    void SetUp() override {
+        std::call_once(init_, call_once_f);
+        init(false, log_dir_); // NOLINT
+    }
 
     void TearDown() override { fin(); }
+
+private:
+    static inline std::once_flag init_; // NOLINT
+    static inline std::string log_dir_; // NOLINT
 };
 
-TEST_F(ongoing_tx_test, simple) { // NOLINT
+TEST_F(ongoing_tx_test, exist_preceding_id_test) { // NOLINT
     ongoing_tx::push({1, 1});
     ASSERT_EQ(ongoing_tx::exist_preceding_id(2), true);
     ongoing_tx::remove_id(1);
     ASSERT_EQ(ongoing_tx::exist_preceding_id(2), false);
+}
+
+TEST_F(ongoing_tx_test, exist_id_test) { // NOLINT
+    ASSERT_EQ(ongoing_tx::exist_id(1), false);
+    ongoing_tx::push({1, 1});
+    ASSERT_EQ(ongoing_tx::exist_id(1), true);
+    ongoing_tx::remove_id(1);
+    ASSERT_EQ(ongoing_tx::exist_id(1), false);
+}
+
+TEST_F(ongoing_tx_test, get_lowest_epoch_test) { // NOLINT
+
+    // register
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 0);
+    ongoing_tx::push({1, 1});
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 1);
+    ongoing_tx::push({2, 2});
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 1);
+    ongoing_tx::push({3, 3});
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 1);
+
+    // delete from old
+    ongoing_tx::remove_id(1);
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 2);
+    ongoing_tx::remove_id(2);
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 3);
+    ongoing_tx::remove_id(3);
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 0);
+
+    // register again
+    ongoing_tx::push({1, 1});
+    ongoing_tx::push({2, 2});
+    ongoing_tx::push({3, 3});
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 1);
+
+    // delete from new
+    ongoing_tx::remove_id(3);
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 1);
+    ongoing_tx::remove_id(2);
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 1);
+    ongoing_tx::remove_id(1);
+    ASSERT_EQ(ongoing_tx::get_lowest_epoch(), 0);
 }
 
 } // namespace shirakami::testing
