97579c9ed9dd04d6dbfdfc2e7bd3666b79974857
Takayuki Tanabe <thawk105@gmail.com>
2022-04-07 15:36:09 +0900

    add next test about with invalid handle and for two.

--- test/concurrency_control/common/scan/c_next_test.cpp
+++ test/concurrency_control/common/scan/c_next_test.cpp
@@ -4,6 +4,16 @@
 
 #include "concurrency_control/include/tuple_local.h"
 
+#ifdef WP
+
+#include "concurrency_control/wp/include/session.h"
+
+#else
+
+#include "concurrency_control/silo/include/session.h"
+
+#endif
+
 #include "shirakami/interface.h"
 
 #include "glog/logging.h"
@@ -42,6 +52,42 @@ TEST_F(c_next, next_with_not_begin) { // NOLINT
     Token s{};
     ASSERT_EQ(Status::OK, enter(s));
     ASSERT_EQ(Status::WARN_NOT_BEGIN, next(s, {}));
+    ASSERT_EQ(Status::OK, leave(s));
+}
+
+TEST_F(c_next, next_with_invalid_handle) { // NOLINT
+    Storage st{};
+    register_storage(st);
+    Token s{};
+    ASSERT_EQ(Status::OK, enter(s));
+    ASSERT_EQ(Status::OK, tx_begin(s));
+    ASSERT_EQ(Status::WARN_INVALID_HANDLE, next(s, {}));
+    ASSERT_EQ(Status::OK, leave(s));
+}
+
+TEST_F(c_next, next_for_two) { // NOLINT
+    Storage st{};
+    register_storage(st);
+    Token s{};
+    ASSERT_EQ(Status::OK, enter(s));
+
+    // prepare data
+    std::string k1{"k1"};
+    std::string k2{"k2"};
+    ASSERT_EQ(Status::OK, upsert(s, st, k1, ""));
+    ASSERT_EQ(Status::OK, upsert(s, st, k2, ""));
+    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+    // test
+    ScanHandle hd{};
+    ASSERT_EQ(Status::OK, open_scan(s, st, "", scan_endpoint::INF, "",
+                                    scan_endpoint::INF, hd));
+    session* ti = static_cast<session*>(s);
+    auto before_next{ti->get_scan_handle().get_scan_cache_itr()[hd]};
+    ASSERT_EQ(Status::OK, next(s, hd));
+    auto after_next{ti->get_scan_handle().get_scan_cache_itr()[hd]};
+    ASSERT_NE(before_next, after_next);
+    ASSERT_EQ(Status::WARN_SCAN_LIMIT, next(s, hd));
     ASSERT_EQ(Status::OK, leave(s));
 }
 
