858c93ae827e7d30180ab36f709822605cf87fb8
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2022-07-06 22:21:44 +0900

    add test storage recovery with no records

---
+++ test/datastore/limestone_integration_storage_recovery_test.cpp
@@ -0,0 +1,91 @@
+
+#include <xmmintrin.h>
+
+#include <array>
+#include <atomic>
+#include <mutex>
+#include <string_view>
+#include <thread>
+#include <vector>
+
+#include "atomic_wrapper.h"
+#include "clock.h"
+#include "test_tool.h"
+#include "tsc.h"
+
+#include "concurrency_control/wp/include/epoch.h"
+#include "concurrency_control/wp/include/lpwal.h"
+#include "concurrency_control/wp/include/record.h"
+#include "concurrency_control/wp/include/version.h"
+
+#include "shirakami/interface.h"
+
+#include "yakushima/include/kvs.h"
+
+#include "gtest/gtest.h"
+
+#include "glog/logging.h"
+
+#include "boost/filesystem.hpp"
+#include "boost/foreach.hpp"
+
+namespace shirakami::testing {
+
+using namespace shirakami;
+
+class limestone_integration_storage_recovery_test
+    : public ::testing::Test { // NOLINT
+public:
+    static void call_once_f() {
+        google::InitGoogleLogging("shirakami-test-data_store-"
+                                  "limestone_integration_storage_recovery_test");
+        FLAGS_stderrthreshold = 0;
+    }
+
+    void SetUp() override { std::call_once(init_google, call_once_f); }
+
+    void TearDown() override {}
+
+private:
+    static inline std::once_flag init_google; // NOLINT
+};
+
+void recovery_test() {
+    // start
+    std::string log_dir{};
+    int tid = syscall(SYS_gettid); // NOLINT
+    std::uint64_t tsc = rdtsc();
+    log_dir =
+            "/tmp/shirakami-" + std::to_string(tid) + "-" + std::to_string(tsc);
+    init({database_options::open_mode::CREATE, log_dir}); // NOLINT
+
+    // storage creation
+    Storage st{};
+    ASSERT_EQ(Status::OK, register_storage(st));
+
+    fin(false);
+
+    // start
+    init({database_options::open_mode::RESTORE, log_dir}); // NOLINT
+
+    // test: log exist
+    Token s{};
+    ASSERT_EQ(Status::OK, enter(s));
+    // test: check recovery
+    std::string vb{};
+    std::vector<Storage> st_list{};
+    ASSERT_EQ(Status::OK, list_storage(st_list));
+    EXPECT_EQ(st_list.size(), 1);
+
+    ASSERT_EQ(Status::WARN_NOT_FOUND, search_key(s, st, "", vb));
+    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+    ASSERT_EQ(Status::OK, leave(s));
+    fin();
+}
+
+TEST_F(limestone_integration_storage_recovery_test, registered_storage_with_no_records) { // NOLINT
+    ASSERT_NO_FATAL_FAILURE(recovery_test());                        // NOLINT
+}
+
+} // namespace shirakami::testing
