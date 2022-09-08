2a2aace56bf6baeaf5c103b70de61107bed1e17e
Takayuki Tanabe <thawk105@gmail.com>
2022-09-08 14:13:50 +0900

    fix api about sequence to receive token info

--- include/shirakami/interface.h
+++ include/shirakami/interface.h
@@ -8,6 +8,7 @@
 #include <vector>
 
 #include "api_storage.h"
+#include "api_sequence.h"
 #include "database_options.h"
 #include "log_record.h"
 #include "scheme.h"
@@ -419,89 +420,6 @@ extern Status update(Token token, Storage storage, std::string_view key,
 extern Status upsert(Token token, Storage storage, std::string_view key,
                      std::string_view val); // NOLINT
 
-
-/**
- * About sequence function
- */
-
-/**
- * @brief sequence id
- * @details the identifier that uniquely identifies the sequence in the database
- */
-using SequenceId = std::size_t;
-
-/**
- * @brief sequence value
- * @details the value of the sequence. Each value in the sequence is associated 
- * with some version number.
- */
-using SequenceValue = std::int64_t;
-
-/**
- * @brief sequence version
- * @details the version number of the sequence that begins at 0 and increases monotonically.
- * For each version in the sequence, there is the associated value with it.
- */
-using SequenceVersion = std::size_t;
-
-/**
- * @brief create new sequence
- * @param [out] id the newly assigned sequence id, that is valid only 
- * when this function is successful with Status::OK.
- * @return Status::OK if the creation was successful
- * @return otherwise if any error occurs
- * @note This function is not intended to be called concurrently with running transactions.
- */
-extern Status create_sequence(SequenceId* id);
-
-/**
- * @brief update sequence value and version
- * @details request shirakami to make the sequence value for the specified version 
- * durable together with the associated transaction.
- * @param token the session token whose current transaction will be associated 
- * with the sequence value and version
- * @param id the sequence id whose value/version will be updated
- * @param version the version of the sequence value
- * @param value the new sequence value
- * @return Status::OK if the update operation is successful
- * @return otherwise if any error occurs
- * @warning multiple update_sequence calls to a sequence with same version number 
- * cause undefined behavior.
- */
-extern Status update_sequence(Token token, SequenceId id,
-                              SequenceVersion version, SequenceValue value);
-
-/**
- * @brief read sequence value
- * @details retrieve sequence value of the "latest" version from shirakami
- * Shirakami determines the latest version by finding maximum version number of
- * the sequence from the transactions that are durable at the time this function call is made.
- * It's up to shirakami when to make transactions durable, so there can be 
- * delay of indeterminate length before update operations become visible to this function. 
- * As for concurrent update operations, it's only guaranteed that the version number 
- * retrieved by this function is equal or greater than the one that is previously retrieved.
- * @param id the sequence id whose value/version are to be retrieved
- * @param [out] version the sequence's latest version number, that is valid only 
- * when this function is successful with Status::OK.
- * @param [out] value the sequence value, that is valid only when this function 
- * is successful with Status::OK.
- * @return Status::OK if the retrieval is successful
- * @return otherwise if any error occurs
- * @note This function is not intended to be called concurrently with running transactions.
- * Typical usage is to retrieve sequence initial value at the time of database recovery.
- */
-extern Status read_sequence(SequenceId id, SequenceVersion* version,
-                            SequenceValue* value);
-
-/**
- * @brief delete the sequence
- * @param[in] id the sequence id that will be deleted
- * @return Status::OK if the deletion was successful
- * @return otherwise if any error occurs
- * @note This function is not intended to be called concurrently with running transactions.
- * Typical usage is in DDL to unregister sequence objects.
- */
-extern Status delete_sequence(SequenceId id);
 
 //==========
 /**
