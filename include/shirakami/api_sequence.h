d53bafcdf1834d933d929628f084455936bbdb19
Takayuki Tanabe <thawk105@gmail.com>
2022-09-12 14:00:40 +0900

    update about sequence api

--- include/shirakami/api_sequence.h
+++ include/shirakami/api_sequence.h
@@ -21,71 +21,77 @@ using SequenceValue = std::int64_t;
 
 /**
  * @brief sequence version
- * @details the version number of the sequence that begins at 0 and increases monotonically.
- * For each version in the sequence, there is the associated value with it.
+ * @details the version number of the sequence that begins at 0 and increases 
+ * monotonically. For each version in the sequence, there is the associated 
+ * value with it.
  */
 using SequenceVersion = std::size_t;
 
 /**
  * @brief create new sequence
- * @param[in] token
  * @param [out] id the newly assigned sequence id, that is valid only 
+ * @param[in] token
  * when this function is successful with Status::OK.
  * @return Status::OK if the creation was successful
  * @return otherwise if any error occurs
- * @note This function is not intended to be called concurrently with running transactions.
+ * @note This function is not intended to be called concurrently with running 
+ * transactions.
  */
-extern Status create_sequence(Token token, SequenceId* id);
+extern Status create_sequence(SequenceId* id, Token token = nullptr); // NOLINT
 
 /**
  * @brief update sequence value and version
- * @details request shirakami to make the sequence value for the specified version 
- * durable together with the associated transaction.
- * @param[in] token the session token whose current transaction will be associated 
- * with the sequence value and version
+ * @details request shirakami to make the sequence value for the specified 
+ * version durable together with the associated transaction.
  * @param[in] id the sequence id whose value/version will be updated
  * @param[in] version the version of the sequence value
  * @param[in] value the new sequence value
+ * @param[in] token the session token whose current transaction will be 
+ * associated with the sequence value and version
  * @return Status::OK if the update operation is successful
  * @return otherwise if any error occurs
- * @warning multiple update_sequence calls to a sequence with same version number 
- * cause undefined behavior.
+ * @warning multiple update_sequence calls to a sequence with same version 
+ * number cause undefined behavior.
  */
-extern Status update_sequence(Token token, SequenceId id,
-                              SequenceVersion version, SequenceValue value);
+extern Status update_sequence(SequenceId id, SequenceVersion version,
+                              SequenceValue value,
+                              Token token = nullptr); // NOLINT
 
 /**
  * @brief read sequence value
  * @details retrieve sequence value of the "latest" version from shirakami
  * Shirakami determines the latest version by finding maximum version number of
- * the sequence from the transactions that are durable at the time this function call is made.
- * It's up to shirakami when to make transactions durable, so there can be 
- * delay of indeterminate length before update operations become visible to this function. 
- * As for concurrent update operations, it's only guaranteed that the version number 
- * retrieved by this function is equal or greater than the one that is previously retrieved.
- * @param[in] token
+ * the sequence from the transactions that are durable at the time this 
+ * function call is made. It's up to shirakami when to make transactions 
+ * durable, so there can be delay of indeterminate length before update 
+ * operations become visible to this function. As for concurrent update 
+ * operations, it's only guaranteed that the version number retrieved by this 
+ * function is equal or greater than the one that is previously retrieved.
  * @param[in] id the sequence id whose value/version are to be retrieved
- * @param [out] version the sequence's latest version number, that is valid only 
- * when this function is successful with Status::OK.
+ * @param [out] version the sequence's latest version number, that is valid 
+ * only when this function is successful with Status::OK.
  * @param [out] value the sequence value, that is valid only when this function 
  * is successful with Status::OK.
+ * @param[in] token
  * @return Status::OK if the retrieval is successful
  * @return otherwise if any error occurs
- * @note This function is not intended to be called concurrently with running transactions.
- * Typical usage is to retrieve sequence initial value at the time of database recovery.
+ * @note This function is not intended to be called concurrently with running 
+ * transactions. Typical usage is to retrieve sequence initial value at the 
+ * time of database recovery.
  */
-extern Status read_sequence(Token token, SequenceId id,
-                            SequenceVersion* version, SequenceValue* value);
+extern Status read_sequence(SequenceId id, SequenceVersion* version,
+                            SequenceValue* value,
+                            Token token = nullptr); // NOLINT
 
 /**
  * @brief delete the sequence
- * @param[in] token
  * @param[in] id the sequence id that will be deleted
+ * @param[in] token
  * @return Status::OK if the deletion was successful
  * @return otherwise if any error occurs
- * @note This function is not intended to be called concurrently with running transactions.
- * Typical usage is in DDL to unregister sequence objects.
+ * @note This function is not intended to be called concurrently with running 
+ * transactions. Typical usage is in DDL to unregister sequence objects.
  */
-extern Status delete_sequence(Token token, SequenceId id);
+extern Status delete_sequence(SequenceId id, Token token = nullptr); // NOLINT
 
 } // namespace shirakami
--- src/concurrency_control/wp/sequence.cpp
+++ src/concurrency_control/wp/sequence.cpp
@@ -6,20 +6,28 @@
 
 namespace shirakami {
 
-Status create_sequence([[maybe_unused]] SequenceId* id) {
+Status create_sequence([[maybe_unused]] SequenceId* id,
+                       [[maybe_unused]] Token token) {
     return Status::OK;
 }
 
-Status update_sequence([[maybe_unused]] Token token, [[maybe_unused]] SequenceId id, [[maybe_unused]] SequenceVersion version, [[maybe_unused]] SequenceValue value) {
+Status update_sequence([[maybe_unused]] SequenceId id,
+                       [[maybe_unused]] SequenceVersion version,
+                       [[maybe_unused]] SequenceValue value,
+                       [[maybe_unused]] Token token) {
     return Status::OK;
 }
 
 
-Status read_sequence([[maybe_unused]] SequenceId id, [[maybe_unused]] SequenceVersion* version, [[maybe_unused]] SequenceValue* value) {
+Status read_sequence([[maybe_unused]] SequenceId id,
+                     [[maybe_unused]] SequenceVersion* version,
+                     [[maybe_unused]] SequenceValue* value,
+                     [[maybe_unused]] Token token) {
     return Status::OK;
 }
 
-Status delete_sequence([[maybe_unused]] SequenceId id) {
+Status delete_sequence([[maybe_unused]] SequenceId id,
+                       [[maybe_unused]] Token token) {
     return Status::OK;
 }
 
