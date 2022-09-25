c368c7b2817a02ae7899340837052d5f3f86c398
Takayuki Tanabe <thawk105@gmail.com>
2022-09-26 08:50:43 +0900

    add test read and boundary wait, boundary abort with epoch / read abort with epoch

---
+++ test/concurrency_control/function/read_and_boundary_wait/read_and_boundary_wait_boundary_abort_with_epoch_test.cpp
@@ -0,0 +1,151 @@
+
+#include <xmmintrin.h>
+
+#include <array>
+#include <atomic>
+#include <mutex>
+#include <string_view>
+#include <thread>
+#include <vector>
+
+#include "atomic_wrapper.h"
+#include "test_tool.h"
+
+#include "concurrency_control/wp/include/epoch.h"
+#include "concurrency_control/wp/include/record.h"
+#include "concurrency_control/wp/include/session.h"
+#include "concurrency_control/wp/include/tuple_local.h"
+#include "concurrency_control/wp/include/version.h"
+
+#include "shirakami/interface.h"
+
+#include "yakushima/include/kvs.h"
+
+#include "gtest/gtest.h"
+
+#include "glog/logging.h"
+
+namespace shirakami::testing {
+
+using namespace shirakami;
+
+class read_and_boundary_wait_boundary_abort_with_epoch_test
+    : public ::testing::Test { // NOLINT
+public:
+    static void call_once_f() {
+        google::InitGoogleLogging("shirakami-test-concurrency_control-function-"
+                                  "read_and_boundary_wait-read_and_boundary_"
+                                  "wait_boundary_abort_with_epoch_test");
+        FLAGS_stderrthreshold = 0;
+    }
+
+    void SetUp() override {
+        std::call_once(init_google_, call_once_f);
+        init(); // NOLINT
+    }
+
+    void TearDown() override { fin(); }
+
+private:
+    static inline std::once_flag init_google_; // NOLINT
+};
+
+TEST_F(read_and_boundary_wait_boundary_abort_with_epoch_test, // NOLINT
+       boundary_abort_with_epoch) {                           // NOLINT
+                                                              // create table
+    // ==========
+    // prepare
+    Storage sta{};
+    Storage stx{};
+    Storage sty{};
+    Storage stz{};
+    ASSERT_EQ(create_storage("a", sta), Status::OK);
+    ASSERT_EQ(create_storage("x", stx), Status::OK);
+    ASSERT_EQ(create_storage("y", sty), Status::OK);
+    ASSERT_EQ(create_storage("z", stz), Status::OK);
+
+    // enter
+    std::array<Token, 5> s{};
+    for (auto&& elem : s) { ASSERT_EQ(enter(elem), Status::OK); }
+
+    // prepare page key value
+    std::string a{"a"};
+    std::string x{"x"};
+    std::string y{"y"};
+    std::string z{"z"};
+    std::array<std::string, 5> var{"t0", "t1", "t2", "t3", "t4"};
+    auto init_db = [&s, &var, a, x, y, z, sta, stx, sty, stz]() {
+        epoch::set_perm_to_proc(epoch::ptp_init_val);
+        ASSERT_EQ(Status::OK, upsert(s.at(0), sta, a, var.at(0)));
+        ASSERT_EQ(Status::OK, upsert(s.at(0), stx, x, var.at(0)));
+        ASSERT_EQ(Status::OK, upsert(s.at(0), sty, y, var.at(0)));
+        ASSERT_EQ(Status::OK, upsert(s.at(0), stz, z, var.at(0)));
+        ASSERT_EQ(Status::OK, commit(s.at(0)));
+    };
+    init_db();
+    std::string buf{};
+    // ==========
+
+    // ==========
+    // note: o is occ, l is ltx
+    // 4l
+    ASSERT_EQ(Status::OK, tx_begin({s.at(1),
+                                    transaction_options::transaction_type::LONG,
+                                    {sta}}));
+    ASSERT_EQ(Status::OK, tx_begin({s.at(2),
+                                    transaction_options::transaction_type::LONG,
+                                    {sty}}));
+    ASSERT_EQ(Status::OK,
+              tx_begin({s.at(3), transaction_options::transaction_type::LONG}));
+    ASSERT_EQ(Status::OK, tx_begin({s.at(4),
+                                    transaction_options::transaction_type::LONG,
+                                    {stz}}));
+    wait_epoch_update();
+    ASSERT_EQ(Status::OK, search_key(s.at(1), stz, z, buf));
+    ASSERT_EQ(buf, var.at(0));
+    ASSERT_EQ(Status::OK, search_key(s.at(2), stx, x, buf));
+    ASSERT_EQ(buf, var.at(0));
+    ASSERT_EQ(Status::OK, search_key(s.at(3), stx, x, buf));
+    ASSERT_EQ(buf, var.at(0));
+    ASSERT_EQ(Status::OK, search_key(s.at(4), sty, y, buf));
+    ASSERT_EQ(buf, var.at(0));
+    ASSERT_EQ(Status::OK, upsert(s.at(4), stz, z, var.at(4)));
+    wait_epoch_update();
+    ASSERT_EQ(Status::OK, upsert(s.at(2), sty, y, var.at(2)));
+    ASSERT_EQ(Status::OK, upsert(s.at(1), sta, a, var.at(1)));
+    ASSERT_EQ(Status::OK, commit(s.at(1)));
+    wait_epoch_update();
+    ASSERT_EQ(Status::OK, search_key(s.at(2), sta, a, buf));
+    ASSERT_EQ(buf, var.at(0));
+    ASSERT_EQ(Status::OK, commit(s.at(2)));
+    wait_epoch_update();
+    ASSERT_EQ(Status::OK, search_key(s.at(3), stz, z, buf));
+    ASSERT_EQ(buf, var.at(0));
+    ASSERT_EQ(Status::OK, commit(s.at(3)));
+    ASSERT_EQ(Status::ERR_VALIDATION, commit(s.at(4)));
+    auto tri = transaction_result_info(s.at(4));
+    ASSERT_EQ((*tri).get_reason_code(), reason_code::COMMITTED_READ_PROTECTION);
+
+    // verify
+    ASSERT_EQ(Status::OK, search_key(s.at(0), sta, a, buf));
+    ASSERT_EQ(buf, var.at(1));
+    ASSERT_EQ(Status::OK, search_key(s.at(0), stx, x, buf));
+    ASSERT_EQ(buf, var.at(0));
+    ASSERT_EQ(Status::OK, search_key(s.at(0), sty, y, buf));
+    ASSERT_EQ(buf, var.at(2));
+    ASSERT_EQ(Status::OK, search_key(s.at(0), stz, z, buf));
+    ASSERT_EQ(buf, var.at(0));
+    ASSERT_EQ(Status::OK, commit(s.at(0)));
+
+    // cleanup
+    init_db();
+
+    // ==========
+
+    // ==========
+    // cleanup
+    for (auto&& elem : s) { ASSERT_EQ(leave(elem), Status::OK); }
+    // ==========
+}
+
+} // namespace shirakami::testing
