e39ce6b6313ce2445098d788f0e1eb51d1046b51
Takayuki Tanabe <thawk105@gmail.com>
2022-10-05 09:58:27 +0900

    add new test file: limestone_integration_no_write_between_two_recovery_test

---
+++ test/datastore/limestone_integration_no_write_between_two_recovery_test.cpp
@@ -0,0 +1,100 @@
+
+#include <xmmintrin.h>
+
+#include <array>
+#include <atomic>
+#include <mutex>
+#include <string_view>
+#include <thread>
+#include <vector>
+
+#include "atomic_wrapper.h"
+#include "clock.h"
+#include "storage.h"
+#include "test_tool.h"
+#include "tsc.h"
+
+#include "concurrency_control/wp/include/epoch.h"
+#include "concurrency_control/wp/include/lpwal.h"
+#include "concurrency_control/wp/include/record.h"
+#include "concurrency_control/wp/include/version.h"
+
+#include "shirakami/interface.h"
+
+#include "yakushima/include/kvs.h"
+
+#include "gtest/gtest.h"
+
+#include "glog/logging.h"
+
+#include "boost/filesystem.hpp"
+#include "boost/foreach.hpp"
+
+namespace shirakami::testing {
+
+using namespace shirakami;
+
+class limestone_integration_single_recovery_test
+    : public ::testing::Test { // NOLINT
+public:
+    static void call_once_f() {
+        google::InitGoogleLogging(
+                "shirakami-test-data_store-"
+                "limestone_integration_no_write_between_two_recovery_test");
+        FLAGS_stderrthreshold = 0;
+    }
+
+    void SetUp() override { std::call_once(init_google, call_once_f); }
+
+    void TearDown() override {}
+
+private:
+    static inline std::once_flag init_google; // NOLINT
+};
+
+std::string create_log_dir_name() {
+    int tid = syscall(SYS_gettid); // NOLINT
+    std::uint64_t tsc = rdtsc();
+    return "/tmp/shirakami-" + std::to_string(tid) + "-" + std::to_string(tsc);
+}
+
+TEST_F(limestone_integration_single_recovery_test, // NOLINT
+       no_write_between_two_recovery) {            // NOLINT
+    // prepare
+    std::string log_dir{};
+    log_dir = create_log_dir_name();
+    init({database_options::open_mode::CREATE, log_dir}); // NOLINT
+
+    Storage st{};
+    ASSERT_EQ(Status::OK, create_storage("1", st));
+    Token s{};
+    ASSERT_EQ(Status::OK, enter(s));
+    ASSERT_EQ(Status::OK, upsert(s, st, "a", "A"));
+    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+    ASSERT_EQ(Status::OK, leave(s));
+
+    fin(false);
+    init({database_options::open_mode::RESTORE, log_dir}); // NOLINT
+    fin(false);
+    init({database_options::open_mode::RESTORE, log_dir}); // NOLINT
+    // two recovery
+
+    // test: storage num
+    std::vector<Storage> st_list{};
+    ASSERT_EQ(Status::OK, storage::list_storage(st_list));
+    ASSERT_EQ(st_list.size(), 1); // because single recovery
+    ASSERT_EQ(Status::OK, enter(s));
+
+    // test: contents
+    std::string vb{};
+    ASSERT_EQ(Status::OK, get_storage("1", st));
+    ASSERT_EQ(Status::OK, search_key(s, st, "a", vb));
+    ASSERT_EQ(vb, "A");
+    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+    // cleanup
+    ASSERT_EQ(Status::OK, leave(s));
+    fin(false);
+}
+
+} // namespace shirakami::testing
