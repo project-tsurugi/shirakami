60fcf2eccabcf907a1583288ebae049ea8aaf297
Takayuki Tanabe <thawk105@gmail.com>
2022-11-10 15:43:38 +0900

    improve short scan upsert test

--- test/concurrency_control/short_tx/scan_upsert/short_scan_upsert_test.cpp
+++ test/concurrency_control/short_tx/scan_upsert/short_scan_upsert_test.cpp
@@ -99,22 +99,115 @@ TEST_F(scan_upsert, upsert_after_range_read_same_tx) { // NOLINT
 
 // start: serial two tx
 TEST_F(scan_upsert, range_read_tx_after_upsert_tx) { // NOLINT
+// prepare
     Storage st{};
     ASSERT_EQ(Status::OK, create_storage("", st));
     std::string k("k"); // NOLINT
     std::string v("v"); // NOLINT
     Token s{};
     ASSERT_EQ(Status::OK, enter(s));
+
+    // test, upsert tx
     ASSERT_EQ(upsert(s, st, k, v), Status::OK);
     ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+    // range read tx
     ScanHandle handle{};
     ASSERT_EQ(Status::OK, open_scan(s, st, "", scan_endpoint::INF, "",
                                     scan_endpoint::INF, handle));
     std::string sb{};
     ASSERT_EQ(Status::OK, read_key_from_scan(s, handle, sb));
     ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+    // cleanup
+    ASSERT_EQ(Status::OK, leave(s));
 }
 
 // end: serial two tx
+
+// start: concurrent two tx
+TEST_F(scan_upsert,
+       concurrent_range_read_tx_and_upsert_is_insert_tx) { // NOLINT
+       // prepare
+    Storage st{};
+    ASSERT_EQ(Status::OK, create_storage("", st));
+    std::string a("a"); // NOLINT
+    std::string b("b"); // NOLINT
+    Token s{};
+    ASSERT_EQ(Status::OK, enter(s));
+    ASSERT_EQ(upsert(s, st, a, a), Status::OK);
+    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+    // start test, scan
+    ScanHandle handle{};
+    ASSERT_EQ(Status::OK, open_scan(s, st, "", scan_endpoint::INF, "",
+                                    scan_endpoint::INF, handle));
+    std::string sb{};
+    ASSERT_EQ(Status::OK, read_key_from_scan(s, handle, sb));
+    ASSERT_EQ(sb, a);
+    ASSERT_EQ(Status::OK, read_value_from_scan(s, handle, sb));
+    ASSERT_EQ(sb, a);
+    ASSERT_EQ(next(s, handle), Status::WARN_SCAN_LIMIT);
+    ASSERT_EQ(close_scan(s, handle), Status::OK);
+
+    // upsert tx
+    Token s2{};
+    ASSERT_EQ(Status::OK, enter(s2));
+    ASSERT_EQ(upsert(s2, st, b, b), Status::OK);
+    ASSERT_EQ(Status::OK, commit(s2)); // NOLINT
+
+    // commit scan tx
+    ASSERT_EQ(Status::ERR_PHANTOM, commit(s)); // NOLINT
+
+    // cleanup
+    ASSERT_EQ(Status::OK, leave(s));
+    ASSERT_EQ(Status::OK, leave(s2));
+}
+
+TEST_F(scan_upsert,
+       concurrent_range_read_tx_and_upsert_is_update_tx) { // NOLINT
+       // prepare
+    Storage st{};
+    ASSERT_EQ(Status::OK, create_storage("", st));
+    std::string a("a"); // NOLINT
+    std::string b("b"); // NOLINT
+    Token s{};
+    ASSERT_EQ(Status::OK, enter(s));
+    ASSERT_EQ(upsert(s, st, a, a), Status::OK);
+    ASSERT_EQ(upsert(s, st, b, b), Status::OK);
+    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+    // start test, scan
+    ScanHandle handle{};
+    ASSERT_EQ(Status::OK, open_scan(s, st, "", scan_endpoint::INF, "",
+                                    scan_endpoint::INF, handle));
+    std::string sb{};
+    ASSERT_EQ(Status::OK, read_key_from_scan(s, handle, sb));
+    ASSERT_EQ(sb, a);
+    ASSERT_EQ(Status::OK, read_value_from_scan(s, handle, sb));
+    ASSERT_EQ(sb, a);
+    ASSERT_EQ(next(s, handle), Status::OK);
+    ASSERT_EQ(Status::OK, read_key_from_scan(s, handle, sb));
+    ASSERT_EQ(sb, b);
+    ASSERT_EQ(Status::OK, read_value_from_scan(s, handle, sb));
+    ASSERT_EQ(sb, b);
+    ASSERT_EQ(next(s, handle), Status::WARN_SCAN_LIMIT);
+    ASSERT_EQ(close_scan(s, handle), Status::OK);
+
+    // upsert tx
+    Token s2{};
+    ASSERT_EQ(Status::OK, enter(s2));
+    ASSERT_EQ(upsert(s2, st, b, b), Status::OK);
+    ASSERT_EQ(Status::OK, commit(s2)); // NOLINT
+
+    // commit scan tx
+    ASSERT_EQ(Status::ERR_VALIDATION, commit(s)); // NOLINT
+
+    // cleanup
+    ASSERT_EQ(Status::OK, leave(s));
+    ASSERT_EQ(Status::OK, leave(s2));
+}
+
+// end: concurrent two tx
 
 } // namespace shirakami::testing
