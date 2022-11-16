e099838f5efe9284ec09b4842dac8db466133f66
Takayuki Tanabe <thawk105@gmail.com>
2022-11-16 17:37:32 +0900

    improve comp test for resolving issue

--- test/concurrency_control/complicated/comp_test.cpp
+++ test/concurrency_control/complicated/comp_test.cpp
@@ -40,80 +40,186 @@ private:
 };
 
 TEST_F(comp_test, test1) { // NOLINT
-    /**
-     * scenario.
-     * 1: scan read 14 entry
-     * 2: create sequence
-     * 3: read sequence id 15
-     * 4: upsert 14 entry + new 1 entry
-     * 5: get_storage "test" twice, WARN_NOT_FOUND
-     * 6: create_storage "test" and get_storage "test"
-     */
-    // prepare
     Storage st{};
     ASSERT_EQ(Status::OK, create_storage("", st));
-    Token s{};
-    ASSERT_EQ(Status::OK, enter(s));
+    auto drop_table_test = [](Storage test_storage) {
+        /**
+         * L7840-7889 scenario.
+         * 1: tx_begin
+         * 2: delete_storage "test"
+         * 3: get_storage "test" warn not found
+         * 4: commit
+         */
+        Token s{};
+        ASSERT_EQ(Status::OK, enter(s));
 
-    // prepare
-    for (auto i = 0; i < 14; ++i) { // NOLINT
-        std::string key(1, i);
-        std::string value(std::to_string(i));
-        ASSERT_EQ(upsert(s, st, key, value), Status::OK);
-    }
-    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+        // 1
+        ASSERT_EQ(Status::OK,
+                  tx_begin({s, transaction_options::transaction_type::SHORT}));
 
-    // test
-    ScanHandle handle{};
-    ASSERT_EQ(Status::OK, open_scan(s, st, "", scan_endpoint::INF, "",
-                                    scan_endpoint::INF, handle));
-    // read all range
-    for (auto i = 0; i < 14; ++i) { // NOLINT
-        std::string sb{};
-        ASSERT_EQ(Status::OK, read_key_from_scan(s, handle, sb));
-        ASSERT_EQ(sb, std::string(1, i));
-        ASSERT_EQ(Status::OK, read_value_from_scan(s, handle, sb));
-        ASSERT_EQ(sb, std::to_string(i));
-        if (i != 13) { // NOLINT
-            ASSERT_EQ(next(s, handle), Status::OK);
-        } else {
-            ASSERT_EQ(next(s, handle), Status::WARN_SCAN_LIMIT);
+        // 2
+        ASSERT_EQ(delete_storage(test_storage), Status::OK);
+
+        // 3
+        Storage st_out{};
+        ASSERT_EQ(get_storage("test", st_out), Status::WARN_NOT_FOUND);
+
+        // 4
+        ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+        // cleanup
+        ASSERT_EQ(Status::OK, leave(s));
+    };
+    auto create_table_test = [st](SequenceId& sid, Storage& test_storage) {
+        /**
+          * L7843-8040 scenario.
+          * 1: scan read 14 entry
+          * 2: create sequence
+          * 3: read sequence id 15
+          * 4: upsert 14 entry + new 1 entry
+          * 5: get_storage "test" twice, WARN_NOT_FOUND
+          * 6: create_storage "test" and get_storage "test"
+          */
+        // prepare
+        Token s{};
+        ASSERT_EQ(Status::OK, enter(s));
+
+        // prepare
+        for (auto i = 0; i < 14; ++i) { // NOLINT
+            std::string key(1, i);
+            std::string value(std::to_string(i));
+            ASSERT_EQ(upsert(s, st, key, value), Status::OK);
         }
-    }
-    ASSERT_EQ(close_scan(s, handle), Status::OK);
+        ASSERT_EQ(Status::OK, commit(s)); // NOLINT
 
-    // 2
-    SequenceId sid{};
-    ASSERT_EQ(Status::OK, create_sequence(&sid));
+        // test
+        ScanHandle handle{};
+        ASSERT_EQ(Status::OK, open_scan(s, st, "", scan_endpoint::INF, "",
+                                        scan_endpoint::INF, handle));
+        // read all range
+        for (auto i = 0; i < 14; ++i) { // NOLINT
+            std::string sb{};
+            ASSERT_EQ(Status::OK, read_key_from_scan(s, handle, sb));
+            ASSERT_EQ(sb, std::string(1, i));
+            ASSERT_EQ(Status::OK, read_value_from_scan(s, handle, sb));
+            ASSERT_EQ(sb, std::to_string(i));
+            next(s, handle);
+        }
+        ASSERT_EQ(close_scan(s, handle), Status::OK);
 
-    // 3
-    SequenceVersion sver{};
-    SequenceValue sval{};
-    ASSERT_EQ(Status::WARN_NOT_FOUND, read_sequence(15, &sver, &sval));
+        // 2
+        ASSERT_EQ(Status::OK, create_sequence(&sid));
 
-    // 4
-    for (auto i = 0; i < 14; ++i) { // NOLINT
-        std::string key(1, i);
-        std::string value(std::to_string(i));
+        // 3
+        SequenceVersion sver{};
+        SequenceValue sval{};
+        ASSERT_EQ(Status::WARN_NOT_FOUND, read_sequence(15, &sver, &sval));
+
+        // 4
+        for (auto i = 0; i < 14; ++i) { // NOLINT
+            std::string key(1, i);
+            std::string value(std::to_string(i));
+            ASSERT_EQ(upsert(s, st, key, value), Status::OK);
+        }
+        std::string key(1, 14);                // NOLINT
+        std::string value(std::to_string(14)); // NOLINT
         ASSERT_EQ(upsert(s, st, key, value), Status::OK);
+
+        // 5
+        Storage st_out{};
+        ASSERT_EQ(get_storage("test", st_out), Status::WARN_NOT_FOUND);
+        ASSERT_EQ(get_storage("test", st_out), Status::WARN_NOT_FOUND);
+
+        // 6
+        ASSERT_EQ(create_storage("test", st_out), Status::OK);
+        ASSERT_EQ(get_storage("test", st_out), Status::OK);
+        test_storage = st_out;
+
+        ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+        // cleanup
+        ASSERT_EQ(Status::OK, leave(s));
+    };
+    auto insert_into_test = [](SequenceId sid, Storage st) {
+        /**
+         * L7563- L7625 scenario.
+         * 1: tx_begin short
+         * 2: update_sequence token 1, id created before create_table_test,
+         * version 2, value 1.
+         * 3: insert into test
+         * 4: commit
+         */
+
+        // prepare
+        Token s{};
+        ASSERT_EQ(Status::OK, enter(s));
+
+        // 1
+        ASSERT_EQ(Status::OK,
+                  tx_begin({s, transaction_options::transaction_type::SHORT}));
+
+        // 2
+        ASSERT_EQ(Status::OK, update_sequence(s, sid, 2, 1));
+
+        // 3
+        ASSERT_EQ(Status::OK, insert(s, st, "", ""));
+
+        // 4
+        ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+        // cleanup
+        ASSERT_EQ(Status::OK, leave(s));
+    };
+    auto select_int4_from_test = [](Storage st) {
+        /**
+         * L7671- L7779 scenario.
+         * 1: tx_begin short
+         * 2: full scan for test
+         * 3: read key, read value from scan
+         * 4: next, warn scan limit
+         * 5: close scan
+         * 6: commit
+         */
+
+        // prepare
+        Token s{};
+        ASSERT_EQ(Status::OK, enter(s));
+
+        // 1
+        ASSERT_EQ(Status::OK,
+                  tx_begin({s, transaction_options::transaction_type::SHORT}));
+
+        // 2
+        ScanHandle shd{};
+        ASSERT_EQ(Status::OK, open_scan(s, st, "", scan_endpoint::INF, "",
+                                        scan_endpoint::INF, shd));
+
+        // 3
+        std::string buf{};
+        ASSERT_EQ(Status::OK, read_key_from_scan(s, shd, buf));
+        ASSERT_EQ(Status::OK, read_value_from_scan(s, shd, buf));
+
+        // 4
+        ASSERT_EQ(Status::WARN_SCAN_LIMIT, next(s, shd));
+
+        // 5
+        ASSERT_EQ(Status::OK, close_scan(s, shd));
+
+        // 6
+        ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+
+        // cleanup
+        ASSERT_EQ(Status::OK, leave(s));
+    };
+
+    for (auto i = 0; i < 10; ++i) { // NOLINT
+        SequenceId sid{};
+        Storage test_storage{};
+        create_table_test(sid, test_storage);
+        insert_into_test(sid, test_storage);
+        select_int4_from_test(test_storage);
+        drop_table_test(test_storage);
     }
-    std::string key(1, 14);                // NOLINT
-    std::string value(std::to_string(14)); // NOLINT
-    ASSERT_EQ(upsert(s, st, key, value), Status::OK);
-
-    // 5
-    Storage st_out{};
-    ASSERT_EQ(get_storage("test", st_out), Status::WARN_NOT_FOUND);
-    ASSERT_EQ(get_storage("test", st_out), Status::WARN_NOT_FOUND);
-
-    // 6
-    ASSERT_EQ(create_storage("test", st_out), Status::OK);
-    ASSERT_EQ(get_storage("test", st_out), Status::OK);
-
-    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
-
-    // cleanup
-    ASSERT_EQ(Status::OK, leave(s));
 }
 
 TEST_F(comp_test, test2) { // NOLINT
