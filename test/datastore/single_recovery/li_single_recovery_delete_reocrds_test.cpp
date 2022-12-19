4044eda516d5c445569435d68d82304199b217e8
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2022-12-19 22:36:08 +0900

    add testcase to delete record after recovery

---
+++ test/datastore/single_recovery/li_single_recovery_delete_reocrds_test.cpp
@@ -0,0 +1,101 @@
+
+#include <xmmintrin.h>
+
+#include <array>
+#include <atomic>
+#include <mutex>
+#include <string_view>
+#include <thread>
+#include <vector>
+
+#include "atomic_wrapper.h"
+#include "clock.h"
+#include "storage.h"
+#include "test_tool.h"
+#include "tsc.h"
+
+#include "concurrency_control/include/epoch.h"
+#include "concurrency_control/include/lpwal.h"
+#include "concurrency_control/include/record.h"
+#include "concurrency_control/include/version.h"
+
+#include "shirakami/interface.h"
+
+#include "yakushima/include/kvs.h"
+
+#include "gtest/gtest.h"
+
+#include "glog/logging.h"
+
+#include "boost/filesystem.hpp"
+#include "boost/foreach.hpp"
+
+namespace shirakami::testing {
+
+using namespace shirakami;
+
+class li_single_recovery_delete_records_test
+    : public ::testing::Test { // NOLINT
+public:
+    static void call_once_f() {
+        google::InitGoogleLogging("shirakami-test-data_store-"
+                                  "li_single_recovery_delete_records_test");
+        FLAGS_stderrthreshold = 0;
+    }
+
+    void SetUp() override { std::call_once(init_google, call_once_f); }
+
+    void TearDown() override {}
+
+private:
+    static inline std::once_flag init_google; // NOLINT
+};
+
+std::string create_log_dir_name() {
+    int tid = syscall(SYS_gettid); // NOLINT
+    std::uint64_t tsc = rdtsc();
+    return "/tmp/shirakami-" + std::to_string(tid) + "-" + std::to_string(tsc);
+}
+
+TEST_F(li_single_recovery_delete_records_test, // NOLINT
+       two_page_write_one_delete_one_storage) {    // NOLINT
+    // prepare
+    std::string log_dir{};
+    log_dir = create_log_dir_name();
+    LOG(INFO) << ">> first start up";
+    init({database_options::open_mode::CREATE, log_dir}); // NOLINT
+    LOG(INFO) << "<< first start up";
+
+    Storage st{};
+    ASSERT_EQ(Status::OK, create_storage("1", st, {2, ""}));
+    Token s{};
+    ASSERT_EQ(Status::OK, enter(s));
+    ASSERT_EQ(Status::OK, upsert(s, st, "a", "A"));
+    ASSERT_EQ(Status::OK, upsert(s, st, "b", "B"));
+    ASSERT_EQ(Status::OK, upsert(s, st, "c", "C"));
+    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+    ASSERT_EQ(Status::OK, leave(s));
+
+    LOG(INFO) << ">> first shutdown";
+    fin(false);
+    LOG(INFO) << "<< first shutdown";
+    LOG(INFO) << ">> second start up";
+    init({database_options::open_mode::RESTORE, log_dir}); // NOLINT
+    LOG(INFO) << "<< second start up";
+
+    ASSERT_EQ(Status::OK, enter(s));
+    ASSERT_EQ(Status::OK,
+            tx_begin({s, transaction_options::transaction_type::LONG, {st}}));
+    wait_epoch_update();
+    std::string vb{};
+    ASSERT_EQ(Status::OK, search_key(s, st, "b", vb));
+    ASSERT_EQ(Status::OK, delete_record(s, st, "b"));
+    ASSERT_EQ(Status::OK, commit(s)); // NOLINT
+    ASSERT_EQ(Status::OK, leave(s));
+
+    LOG(INFO) << ">> second shutdown";
+    fin(false);
+    LOG(INFO) << "<< second shutdown";
+}
+
+} // namespace shirakami::testing
