574c008e4dc0d65e4b6d45c219e91012064234d3
Takayuki Tanabe <thawk105@gmail.com>
2022-12-28 13:29:14 +0900

    update for tsurugi-issue 148

--- test/concurrency_control/complicated/readme.md
+++ test/concurrency_control/complicated/readme.md
@@ -7,3 +7,14 @@
     - 上記イシューの検証をサポートするためのテスト。
   - test 2
     - 全てLTX. insert 3 件コミット。二番目の挿入したものを削除してコミット。削除したものと同じものを挿入してコミット。再度削除してコミット。スキャンを実行すると２件で意図通り。search を実行すると delete 済みのものが見える。
+
+- tsurugi_issue148.cpp
+  - 以下の処理を行うスレッドを３０並列で実行する。
+    - delete from test2 where  key1 = :key1
+      - key1は主キーの一部なのでrange scanになる
+      - open_scanでrangeにヒットしたものに対してdelete_recordを実行
+    - select foo, bar, baz from test
+      - test表のストレージに対するfull scan
+    - insert into test2 (key1,key2,key3,value1) values(:key1,:key2,:key3,:value1)
+      - test2表のストレージに対するinsertになる
+    - transactionのcommit
---
+++ test/concurrency_control/complicated/tsurugi_issue148.cpp
@@ -0,0 +1,49 @@
+
+#include <glog/logging.h>
+
+#include <mutex>
+
+#include "clock.h"
+
+#include "test_tool.h"
+
+#include "concurrency_control/include/epoch.h"
+#include "concurrency_control/include/session.h"
+#include "concurrency_control/include/tuple_local.h"
+
+#include "shirakami/interface.h"
+
+#include "glog/logging.h"
+#include "gtest/gtest.h"
+
+using namespace shirakami;
+
+namespace shirakami::testing {
+
+class tsurugi_issue148 : public ::testing::Test { // NOLINT
+public:
+    static void call_once_f() {
+        google::InitGoogleLogging("shirakami-test-concurrency_control-"
+                                  "complicated-tsurugi_issue148");
+        FLAGS_stderrthreshold = 0;
+    }
+
+    void SetUp() override {
+        std::call_once(init_, call_once_f);
+        init(); // NOLINT
+    }
+
+    void TearDown() override { fin(); }
+
+private:
+    static inline std::once_flag init_; // NOLINT
+};
+
+TEST_F(tsurugi_issue148, simple) { // NOLINT
+    Storage st_test{};
+    Storage st_test2{};
+    ASSERT_EQ(Status::OK, create_storage("test", st_test));
+    ASSERT_EQ(Status::OK, create_storage("test2", st_test2));
+}
+
+} // namespace shirakami::testing
