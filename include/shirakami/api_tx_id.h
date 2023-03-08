9e10c43e7b73b02dec6d6e66d0e10eee784fa48b
Takayuki Tanabe <thawk105@gmail.com>
2023-03-08 11:38:15 +0900

    improve about tx id

--- include/shirakami/api_tx_id.h
+++ include/shirakami/api_tx_id.h
@@ -8,23 +8,12 @@ namespace shirakami {
 /**
  * @brief Get the tx id object.
  * @param[in] token the transaction handle.
- * @param[out] tx_id output argument. This string is represented by 32 
- * hexadecimal digits.
- * @return Status::OK success.
- * @return Status::WARN_NOT_BEGIN The transaction is not begun.
- * @note If the token is invalid, this cause undefined behavior.
- */
-Status get_tx_id(Token token, std::string& tx_id);
-
-/**
- * @brief Get the tx id object.
- * @param[in] token the transaction handle.
  * @param[out] tx_id output argument. This is composed TID- + 16 hexadecimal 
  * digits, lowercase English by string information.
  * @return Status::OK success.
  * @return Status::WARN_NOT_BEGIN The transaction is not begun.
  * @note If the token is invalid, this cause undefined behavior.
  */
-Status get_tx_id_for_uid(Token token, std::string& tx_id);
+Status get_tx_id(Token token, std::string& tx_id);
 
 } // namespace shirakami
