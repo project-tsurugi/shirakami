fa01be3baf9f598368fc3dfb86bf7d9ca59d618f
Takayuki Tanabe <tanabe@nautilus-technologies.com>
2023-04-17 15:57:54 +0900

    add disabled_test to pass

---
+++ test/concurrency_control/complicated/tsurugi_issue256_test.cpp
@@ -0,0 +1,54 @@
+
+#include <glog/logging.h>
+
+#include <mutex>
+#include <vector>
+
+#include "clock.h"
+
+#include "test_tool.h"
+
+#include "concurrency_control/include/epoch.h"
+#include "concurrency_control/include/session.h"
+#include "concurrency_control/include/tuple_local.h"
+
+#include "shirakami/interface.h"
+
+#include "glog/logging.h"
+#include "gtest/gtest.h"
+
+using namespace shirakami;
+
+#define ASSERT_OK(expr) ASSERT_EQ(expr, shirakami::Status::OK)
+
+namespace shirakami::testing {
+
+class tsurugi_issue256 : public ::testing::Test { // NOLINT
+public:
+    static void call_once_f() {
+        google::InitGoogleLogging("shirakami-test-concurrency_control-"
+                                  "complicated-tsurugi_issue256");
+        FLAGS_stderrthreshold = 0;
+    }
+
+    void SetUp() override {
+        std::call_once(init_, call_once_f);
+        init(); // NOLINT
+    }
+
+    void TearDown() override { fin(); }
+
+private:
+    static inline std::once_flag init_; // NOLINT
+};
+
+TEST_F(tsurugi_issue256, DISABLED_20230412_comment_ban) { // NOLINT
+    Storage st;
+    ASSERT_OK(create_storage("A", st));
+    ::usleep(100 * 1000); // NOLINT
+    ASSERT_OK(delete_storage(st));
+    ::usleep(100 * 1000);               // NOLINT
+    ASSERT_OK(create_storage("B", st)); // heap-use-after-free
+}
+
+} // namespace shirakami::testing
