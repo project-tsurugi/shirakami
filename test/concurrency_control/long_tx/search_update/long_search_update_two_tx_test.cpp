002a6fc6762c9ed973284694d7dd5757d0f9d9a1
Takayuki Tanabe <tanabe@nautilus-technologies.com>
2023-08-28 11:34:34 +0900

    update test

--- test/concurrency_control/long_tx/search_update/long_search_update_two_tx_test.cpp
+++ test/concurrency_control/long_tx/search_update/long_search_update_two_tx_test.cpp
@@ -204,6 +204,62 @@ TEST_F(long_search_update_two_tx_test,               // NOLINT
     ASSERT_EQ(leave(s2), Status::OK);
 }
 
+TEST_F(long_search_update_two_tx_test, // NOLINT
+       concurrent_search_update_txs) { // NOLINT
+    // prepare test
+    Storage st{};
+    ASSERT_EQ(create_storage("", st), Status::OK);
+    Token s1{};
+    Token s2{};
+    ASSERT_EQ(enter(s1), Status::OK);
+    ASSERT_EQ(enter(s2), Status::OK);
+
+    // prepare data
+    ASSERT_EQ(Status::OK,
+              tx_begin({s1, transaction_options::transaction_type::SHORT}));
+    ASSERT_EQ(Status::OK, upsert(s1, st, "", std::to_string(2)));
+    ASSERT_EQ(Status::OK, commit(s1));
+
+    // test
+    ASSERT_EQ(tx_begin({s1, transaction_options::transaction_type::LONG, {st}}),
+              Status::OK);
+    ASSERT_EQ(tx_begin({s2, transaction_options::transaction_type::LONG, {st}}),
+              Status::OK);
+    wait_epoch_update();
+    std::string vb{};
+    ASSERT_EQ(search_key(s1, st, "", vb), Status::OK); // forwarding
+    ASSERT_EQ(vb, std::to_string(2));
+    ASSERT_EQ(search_key(s2, st, "", vb), Status::OK); // forwarding
+    ASSERT_EQ(vb, std::to_string(2));
+    int for_s2 = stoi(vb) + 9; // 11
+    int for_s1 = stoi(vb) + 1; // 3
+    ASSERT_EQ(update(s2, st, "", std::to_string(for_s2)), Status::OK);
+    ASSERT_EQ(Status::WARN_WAITING_FOR_OTHER_TX, commit(s2));
+    ASSERT_EQ(update(s1, st, "", std::to_string(for_s1)), Status::OK);
+    ASSERT_EQ(Status::OK, commit(s1));
+    for (;;) {
+        auto ret = check_commit(s2);
+        if (ret == Status::WARN_WAITING_FOR_OTHER_TX) {
+            _mm_pause();
+            continue;
+        }
+        // s2 は s1 に前置してアボートになる。（read を踏む）
+        if (ret == Status::ERR_CC) { break; } // other status
+        LOG(INFO) << ret;
+        ASSERT_EQ(true, false);
+    }
+
+    // verify
+    ASSERT_EQ(Status::OK,
+              tx_begin({s1, transaction_options::transaction_type::SHORT}));
+    ASSERT_EQ(search_key(s1, st, "", vb), Status::OK);
+    ASSERT_EQ(vb, "3");
+
+    // clean up test
+    ASSERT_EQ(leave(s1), Status::OK);
+    ASSERT_EQ(leave(s2), Status::OK);
+}
+
 // end: concurrent two tx
 
 } // namespace shirakami::testing
