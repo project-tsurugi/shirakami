11cc997a262a6092c0a163775f0a056615f913ca
Takayuki Tanabe <tanabe@nautilus-technologies.com>
2023-08-29 10:54:14 +0900

    fix test

--- test/concurrency_control/long_tx/search_upsert/long_search_upsert_two_tx_test.cpp
+++ test/concurrency_control/long_tx/search_upsert/long_search_upsert_two_tx_test.cpp
@@ -70,17 +70,22 @@ TEST_F(long_search_upsert_two_tx_test, // NOLINT
     wait_epoch_update();
     std::string vb{};
     ASSERT_EQ(search_key(s1, st, "", vb), Status::OK); // forwarding
-    ASSERT_EQ(search_key(s1, st, "", vb), Status::OK);
     ASSERT_EQ(vb, std::to_string(2));                  // NOLINT
     ASSERT_EQ(search_key(s2, st, "", vb), Status::OK); // forwarding
-    ASSERT_EQ(search_key(s2, st, "", vb), Status::OK);
     ASSERT_EQ(vb, std::to_string(2));                  // NOLINT
     int for_s2 = stoi(vb) + 9;                         // 11 // NOLINT
     int for_s1 = stoi(vb) + 1;                         // 3 // NOLINT
+
+    ASSERT_EQ(search_key(s2, st, "", vb), Status::OK);
+    ASSERT_EQ(vb, std::to_string(2));                  // NOLINT
     ASSERT_EQ(upsert(s2, st, "", std::to_string(for_s2)), Status::OK);
     ASSERT_EQ(Status::WARN_WAITING_FOR_OTHER_TX, commit(s2));
+
+    ASSERT_EQ(search_key(s1, st, "", vb), Status::OK);
+    ASSERT_EQ(vb, std::to_string(2));                  // NOLINT
     ASSERT_EQ(upsert(s1, st, "", std::to_string(for_s1)), Status::OK);
     ASSERT_EQ(Status::OK, commit(s1));
+
     for (;;) {
         auto ret = check_commit(s2);
         if (ret == Status::WARN_WAITING_FOR_OTHER_TX) {
