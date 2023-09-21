4661b583212fe8856aa3b2cab818f6539f1f2b07
Takayuki Tanabe <tanabe@nautilus-technologies.com>
2023-09-21 14:50:17 +0900

    fix doc

--- include/shirakami/api_result.h
+++ include/shirakami/api_result.h
@@ -9,6 +9,10 @@ namespace shirakami {
 
 /**
  * @brief get transaction result information after the transaction was aborted.
+ * @details When this function is called after a transaction has been aborted, 
+ * the information about the abort contained in the Token is copied to the heap 
+ * memory object and passed. Therefore, the information returned is available 
+ * and unchanged until the user discards it.
  * @param[in] token transaction handle.
  * @return std::shared_ptr<result_info> 
  */
