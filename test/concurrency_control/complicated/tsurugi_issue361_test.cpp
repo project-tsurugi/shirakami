4fa9e260f58d31efaa25006d287f700c4309ab60
Takayuki Tanabe <tanabe@nautilus-technologies.com>
2024-01-22 18:27:27 +0900

    test: improve test
    project-tsurugi/tsurugi-issues#361

--- test/concurrency_control/complicated/tsurugi_issue361_test.cpp
+++ test/concurrency_control/complicated/tsurugi_issue361_test.cpp
@@ -26,7 +26,7 @@ public:
     static void call_once_f() {
         google::InitGoogleLogging("shirakami-test-concurrency_control-"
                                   "complicated-tsurugi_issue361");
-        FLAGS_stderrthreshold = 0;
+        // FLAGS_stderrthreshold = 0;
     }
 
     void SetUp() override {
@@ -41,8 +41,13 @@ private:
 };
 
 TEST_F(tsurugi_issue361, occ_must_not_read_uncommitted_record) { // NOLINT
-    auto count = [](Storage& st) {
-        std::size_t num{0};
+    std::size_t inserting_num = 0;
+    std::size_t ok_num = 0;
+    auto count = [&inserting_num, &ok_num](Storage& st) {
+        // init
+        inserting_num = 0;
+        ok_num = 0;
+
         Token s2{};
         LOG_IF(FATAL, enter(s2) != Status::OK);
         LOG_IF(FATAL, tx_begin({s2}) != Status::OK);
@@ -55,17 +60,19 @@ TEST_F(tsurugi_issue361, occ_must_not_read_uncommitted_record) { // NOLINT
                 rc = read_key_from_scan(s2, sh, buf);
                 LOG_IF(FATAL, rc != Status::OK &&
                                       rc != Status::WARN_CONCURRENT_INSERT);
-                LOG(INFO) << rc << ", " << buf;
-                ++num;
+                if (rc == Status::OK) {
+                    ++ok_num;
+                } else if (rc == Status::WARN_CONCURRENT_INSERT) {
+                    ++inserting_num;
+                } else {
+                    LOG(FATAL);
+                }
             } while (next(s2, sh) == Status::OK);
-        } else if (rc == Status::WARN_NOT_FOUND) {
-            num = 0;
         } else {
             LOG(FATAL) << "open_scan rc:" << rc;
         }
         LOG_IF(FATAL, commit(s2) != Status::OK);
         LOG_IF(FATAL, leave(s2) != Status::OK);
-        return num;
     };
 
     Storage st{};
@@ -81,7 +88,9 @@ TEST_F(tsurugi_issue361, occ_must_not_read_uncommitted_record) { // NOLINT
      * occ reads "1" as cc protocl but doesn't read as service, so return code
      * was Status::WARN_CONCURRENT_INSERT.
     */
-    ASSERT_EQ(count(st), 1);
+    count(st);
+    ASSERT_EQ(inserting_num, 1);
+    ASSERT_EQ(ok_num, 0);
 
     ASSERT_OK(commit(s)); // 0 -> 1
     ASSERT_OK(leave(s));
@@ -89,7 +98,9 @@ TEST_F(tsurugi_issue361, occ_must_not_read_uncommitted_record) { // NOLINT
     /**
      * occ reads "1" as cc protocl and service. (Status::OK)
     */
-    ASSERT_EQ(count(st), 1);
+    count(st);
+    ASSERT_EQ(inserting_num, 0);
+    ASSERT_EQ(ok_num, 1);
 
     ASSERT_OK(enter(s));
     ASSERT_OK(tx_begin({s}));
@@ -100,7 +111,9 @@ TEST_F(tsurugi_issue361, occ_must_not_read_uncommitted_record) { // NOLINT
      * occ reads "2" as cc protocl but doesn't read as service, so return code
      * was Status::WARN_CONCURRENT_INSERT.
     */
-    ASSERT_EQ(count(st), 2);
+    count(st);
+    ASSERT_EQ(inserting_num, 1);
+    ASSERT_EQ(ok_num, 1);
 
     ASSERT_OK(commit(s)); // 1 -> 2
     ASSERT_OK(leave(s));
@@ -108,7 +121,9 @@ TEST_F(tsurugi_issue361, occ_must_not_read_uncommitted_record) { // NOLINT
     /**
      * occ reads "1" and "2" as cc protocl and service. (Status::OK)
     */
-    ASSERT_EQ(count(st), 2);
+    count(st);
+    ASSERT_EQ(inserting_num, 0);
+    ASSERT_EQ(ok_num, 2);
 }
 
 } // namespace shirakami::testing
