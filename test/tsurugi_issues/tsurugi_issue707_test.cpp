214a7c4ed647ce7811f51cff5e7db321e62b37c2
Takayuki Tanabe <tanabe@nautilus-technologies.com>
2024-04-01 19:01:25 +0900

    test: fix to reduce log

--- test/tsurugi_issues/tsurugi_issue707_test.cpp
+++ test/tsurugi_issues/tsurugi_issue707_test.cpp
@@ -29,7 +29,7 @@ public:
     static void call_once_f() {
         google::InitGoogleLogging("shirakami-test-concurrency_control-"
                                   "complicated-tsurugi_issue707_test");
-        FLAGS_stderrthreshold = 0;
+        // FLAGS_stderrthreshold = 0;
         init_for_test();
     }
 
