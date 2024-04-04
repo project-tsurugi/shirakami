1180aa95ef79d6864f8d29f95a1edefad8488c5e
Takayuki Tanabe <tanabe@nautilus-technologies.com>
2024-04-04 15:22:20 +0900

    fix: improve test tool

--- test/include/test_tool.h
+++ test/include/test_tool.h
@@ -5,11 +5,15 @@
 #include "concurrency_control/include/epoch.h"
 #include "database/include/logging.h"
 
+#include "gtest/gtest.h"
+
 #define ASSERT_OK(expr) ASSERT_EQ(expr, shirakami::Status::OK)
 
 namespace shirakami {
 
 static inline void init_for_test() { set_is_debug_mode(true); }
+
+// about epoch
 
 static inline void wait_epoch_update() {
     auto ce{epoch::get_global_epoch()};
@@ -33,5 +37,18 @@ static inline void stop_epoch() {
 }
 
 static inline void resume_epoch() { epoch::set_perm_to_proc(-1); }
+
+// about ltx
+
+static inline void ltx_begin_wait(Token t) {
+    TxStateHandle thd{};
+    ASSERT_OK(acquire_tx_state_handle(t, thd));
+    TxState ts{};
+    do {
+        ASSERT_OK(check_tx_state(thd, ts));
+        std::this_thread::yield();
+    } while (ts.state_kind() == TxState::StateKind::WAITING_START);
+    ASSERT_OK(release_tx_state_handle(thd));
+}
 
 } // namespace shirakami
