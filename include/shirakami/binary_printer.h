d72e4486bff8cb86f5d850543444ae9869d159b8
Nobuhiro Ban <ban@nautilus-technologies.com>
2024-04-29 15:21:50 +0900

    add missing include (for FTBFS with gcc-13)

--- include/shirakami/binary_printer.h
+++ include/shirakami/binary_printer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 tsurugi project.
+ * Copyright 2018-2024 tsurugi project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 #pragma once
 
+#include <cstdint>
 #include <iomanip>
 
 namespace shirakami {
