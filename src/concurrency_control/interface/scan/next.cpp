648e72fad5855cc15db4ca5d172ee4326221881a
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2024-06-14 11:34:20 +0900

    fix broken indentation

--- src/concurrency_control/interface/scan/next.cpp
+++ src/concurrency_control/interface/scan/next.cpp
@@ -66,9 +66,9 @@ Status next_body(Token const token, ScanHandle const handle) { // NOLINT
             inws = ti->get_write_set().search(rec_ptr);
             if (inws != nullptr) {
                 /**
-             * If it exists and it is not delete operation, read from scan api 
-             * call should be able to read the record.
-             */
+                 * If it exists and it is not delete operation, read from scan api 
+                 * call should be able to read the record.
+                 */
                 if (inws->get_op() == OP_TYPE::DELETE) { continue; }
                 break;
             }
