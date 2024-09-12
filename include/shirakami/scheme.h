a1a364519ad98c05f971b55f1f66bbc1c82298e8
Nobuhiro Ban <ban@nautilus-technologies.com>
2024-09-12 17:51:07 +0900

    introduce new OP_TYPEs

--- include/shirakami/scheme.h
+++ include/shirakami/scheme.h
@@ -344,10 +344,12 @@ enum class OP_TYPE : std::int32_t {
     BEGIN,
     COMMIT,
     DELETE,
+    DELSERT,
     INSERT,
     NONE,
     SCAN,
     SEARCH,
+    TOMBSTONE,
     UPDATE,
     UPSERT,
 };
@@ -363,6 +365,8 @@ inline constexpr std::string_view to_string_view(const OP_TYPE op) noexcept {
             return "COMMIT"sv;
         case OP_TYPE::DELETE:
             return "DELETE"sv;
+        case OP_TYPE::DELSERT:
+            return "DELSERT"sv;
         case OP_TYPE::INSERT:
             return "INSERT"sv;
         case OP_TYPE::NONE:
@@ -371,6 +375,8 @@ inline constexpr std::string_view to_string_view(const OP_TYPE op) noexcept {
             return "SCAN"sv;
         case OP_TYPE::SEARCH:
             return "SEARCH"sv;
+        case OP_TYPE::TOMBSTONE:
+            return "TOMBSTONE"sv;
         case OP_TYPE::UPDATE:
             return "UPDATE"sv;
         case OP_TYPE::UPSERT:
