23617992c6decff06b6654b3bbd59dae77211c0e
Nobuhiro Ban <ban@nautilus-technologies.com>
2024-09-13 15:16:31 +0900

    follow-test

--- test/concurrency_control/session_test.cpp
+++ test/concurrency_control/session_test.cpp
@@ -62,7 +62,7 @@ TEST_F(session_test, member_operating) { // NOLINT
     ASSERT_EQ(ti->get_operating(), 0);
     ASSERT_EQ(Status::OK, upsert(s, st, "k", ""));
     ASSERT_EQ(ti->get_operating(), 0);
-    ASSERT_EQ(Status::WARN_CANCEL_PREVIOUS_INSERT, delete_record(s, st, "k"));
+    ASSERT_EQ(Status::OK, delete_record(s, st, "k"));
     ASSERT_EQ(ti->get_operating(), 0);
     std::string sb{};
     ASSERT_EQ(Status::OK, search_key(s, st, "", sb));
@@ -123,7 +123,7 @@ TEST_F(session_test, member_step_epoch_after_each_api) { // NOLINT
     wait_change_step_epoch();
     ASSERT_EQ(Status::OK, upsert(s, st, "k", ""));
     wait_change_step_epoch();
-    ASSERT_EQ(Status::WARN_CANCEL_PREVIOUS_INSERT, delete_record(s, st, "k"));
+    ASSERT_EQ(Status::OK, delete_record(s, st, "k"));
     wait_change_step_epoch();
     std::string sb{};
     ASSERT_EQ(Status::OK, search_key(s, st, "", sb));
