b8d334244fe484bb5065a3ec023f78e9bdf5eac5
Nobuhiro Ban <ban@nautilus-technologies.com>
2026-01-18 22:39:47 +0900

    WIP-write_range

--- src/concurrency_control/include/wp_meta.h
+++ src/concurrency_control/include/wp_meta.h
@@ -26,12 +26,6 @@ namespace shirakami::wp {
  */
 class alignas(CACHE_LINE_SIZE) wp_meta {
 public:
-    /**
-     * key: tx id
-     * value: whether write, left key, right key
-     */
-    using wp_write_range_type =
-            std::map<std::size_t, std::tuple<std::string, std::string>>;
 
     wp_meta() { init(); }
 
@@ -40,10 +34,6 @@ public:
     void init();
 
     wp_lock& get_wp_lock() { return wp_lock_; }
-
-    std::shared_mutex& get_mtx_write_range() { return mtx_write_range_; }
-
-    wp_write_range_type& get_write_range() { return write_range_; }
 
     // ==========
 
@@ -54,12 +44,6 @@ private:
      */
     wp_lock wp_lock_;
 
-    // about write range
-    // ==========
-    std::shared_mutex mtx_write_range_;
-
-    wp_write_range_type write_range_;
-    // ==========
 };
 
 } // namespace shirakami::wp
