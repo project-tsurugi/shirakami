99c3b0f212c424d04218fd6a9a40c6ebfbc06851
Nobuhiro Ban <ban@nautilus-technologies.com>
2026-08-25 17:12:57 +0900

    test: add some (disabled) tests for phantom check for insert ops that create masstree new layer

--- test/concurrency_control/short_tx/insert_scan/short_insert_scan_phantom_test.cpp
+++ test/concurrency_control/short_tx/insert_scan/short_insert_scan_phantom_test.cpp
@@ -6,7 +6,7 @@
 
 #include "shirakami/interface.h"
 
-#include "gtest/gtest.h"
+#include "test_tool.h"
 
 #include "glog/logging.h"
 
@@ -111,6 +111,68 @@ TEST_F(insert_scan_phantom_test, scan_find_phantom_by_insert) { // NOLINT
     // cleanup
     ASSERT_EQ(Status::OK, leave(s1));
     ASSERT_EQ(Status::OK, leave(s2));
+}
+
+// issue 1543
+void scan_find_phantom_by_insert_deep_sub(std::string_view s2_key) {
+    // insert operations may create new border nodes.
+    // these nodes must be tracked by node set for phantom check.
+
+    // expect
+    // s1: begin                       -> OK
+    // s2: begin                       -> OK
+    // s1: full scan                   -> OK
+    // s1: insert 1111111122222222a    -> OK
+    // s1: insert 1111111122222222b    -> OK (ensure that new layer is created for suffix-enabled yakushima)
+    // s2: insert 11111111AAA          -> OK (insert to newly created L1 border node)
+    //  OR insert 1111111122222222AAA  -> OK (insert to newly created L2 border node)
+    // s2: commit                      -> OK
+    // s1: commit                      -> ERR_CC by node verify (PHANTOM)
+
+    // prepare
+    Storage st{};
+    ASSERT_OK(create_storage("test", st));
+    Token s1{};
+    Token s2{};
+    ASSERT_OK(enter(s1));
+    ASSERT_OK(enter(s2));
+    ASSERT_OK(tx_begin({s1, transaction_options::transaction_type::SHORT}));
+    ASSERT_OK(upsert(s1, st, "1", ""));
+    ASSERT_OK(commit(s1));
+
+    // test
+    // s1 scan and register node info
+    ASSERT_OK(tx_begin({s1, transaction_options::transaction_type::SHORT}));
+    ScanHandle handle{};
+    ASSERT_OK(open_scan(s1, st, {}, scan_endpoint::INF, {}, scan_endpoint::INF, handle));
+    std::string sb{};
+    ASSERT_OK(read_key_from_scan(s1, handle, sb));
+    ASSERT_EQ(sb, "1");
+    ASSERT_EQ(Status::WARN_SCAN_LIMIT, next(s1, handle));
+    ASSERT_OK(close_scan(s1, handle));
+    // s1 insert long key
+    ASSERT_OK(insert(s1, st, "1111111122222222a", "")); // suffix-enabled yakushima does not create new layer
+    ASSERT_OK(insert(s1, st, "1111111122222222b", "")); // until second long-key put operation
+    // s2 insert and commit
+    ASSERT_OK(tx_begin({s2, transaction_options::transaction_type::SHORT}));
+    ASSERT_OK(insert(s2, st, s2_key, ""));
+    ASSERT_EQ(Status::OK, commit(s2));
+    // s1 commit and find phantom
+    ASSERT_EQ(Status::ERR_CC, commit(s1)) << "s2_key:" << s2_key;
+
+    // cleanup
+    ASSERT_OK(leave(s1));
+    ASSERT_OK(leave(s2));
+}
+
+// issue 1543
+TEST_F(insert_scan_phantom_test, DISABLED_scan_find_phantom_by_insert_deep_l1) {
+    scan_find_phantom_by_insert_deep_sub("11111111AAA");
+}
+
+// issue 1543
+TEST_F(insert_scan_phantom_test, DISABLED_scan_find_phantom_by_insert_deep_l2) {
+    scan_find_phantom_by_insert_deep_sub("1111111122222222AAA");
 }
 
 } // namespace shirakami::testing
